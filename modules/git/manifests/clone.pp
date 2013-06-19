aa1f8fd5b924321f25d4200fbe928bbdc7f543c7
Jeremiah Orem <oremj@oremj.com>
2013-06-19 15:41:43 -0700

    Add support for git clones.

---
+++ modules/git/manifests/clone.pp
@@ -0,0 +1,14 @@
+# $name is used for the clone location.
+define git::clone(
+    $repo
+) {
+    $clone_dir = $name
+
+    exec {
+        "/usr/bin/git clone ${repo} ${clone_dir}":
+            creates => "${clone_dir}/.git";
+
+        "/usr/bin/git config remote.origin.url ${repo}":
+            unless => "test `/usr/bin/git config remote.origin.url` = '${repo}'"
+    }
+}
