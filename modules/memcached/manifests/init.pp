4b9c3fdf17b38b70f7f0431c2890b7c8612f7f72
Jason Thomas <jason@lithiumfox.com>
2013-06-20 13:19:27 -0400

    Fix up memory settings. Notify service on config change

--- modules/memcached/manifests/init.pp
+++ modules/memcached/manifests/init.pp
@@ -5,7 +5,7 @@ class memcached (
     $port = '11211'
 ) {
 
-    $cachesize = inline_template('<%= @memorysize =~ /^(\d+)/; val = ( ( $1.to_i * 1024) / 1.05 ).to_i %>m')
+    $cachesize = inline_template('<%= @memorysize =~ /^(\d+)/; val = ( ( $1.to_i * 1024) / 1.05 ).to_i %>')
 
     package {
         'memcached':
@@ -27,6 +27,7 @@ class memcached (
     file {
         '/etc/sysconfig/memcached':
             ensure  => present,
+            notify  => Service['memcached'],
             content => template('memcached/memcached'),
             require => Package[memcached];
     }
