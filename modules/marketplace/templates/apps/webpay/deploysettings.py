634a58dd7cb0c77fb30287576a73a6f44aef48ab
Jason Thomas <jason@lithiumfox.com>
2013-06-25 16:33:40 -0400

    No load testing for webpay

--- modules/marketplace/templates/apps/webpay/deploysettings.py
+++ modules/marketplace/templates/apps/webpay/deploysettings.py
@@ -7,5 +7,4 @@ CRON_NAME = '<%= cron_name %>'
 GUNICORN = filter(None, '<%= gunicorn %>'.split(';'))
 MULTI_GUNICORN = filter(None, '<%= multi_gunicorn %>'.split(';'))
 CELERY_SERVICE = '<%= celery_service %>'
-LOAD_TESTING = <%= load_testing %>
 UPDATE_REF = <% if update_ref %>'<%= update_ref %>'<% else %>None<% end %>
