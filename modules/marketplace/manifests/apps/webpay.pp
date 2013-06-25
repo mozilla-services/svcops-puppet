11113914ba0941b742a1eb2ff1b6e3a3974bdecf
Jason Thomas <jason@lithiumfox.com>
2013-06-25 15:21:36 -0400

    Add gunicorn_set for webpay

--- modules/marketplace/manifests/apps/webpay.pp
+++ modules/marketplace/manifests/apps/webpay.pp
@@ -8,7 +8,8 @@ define marketplace::apps::webpay(
     $worker_class = 'sync',
     $timeout = '90',
     $environ = '',
-    $newrelic_license_key = ''
+    $newrelic_license_key = '',
+    $gunicorn_set = false, # runs two workers $name-a and $name-b
 ) {
     $app_name = $name
     $gunicorn = "${app_dir}/venv/bin/gunicorn"
@@ -21,14 +22,29 @@ define marketplace::apps::webpay(
         }
     }
 
-    gunicorn::instance {
-        $gunicorn_name:
-            gunicorn  => $gunicorn,
-            port      => $port,
-            workers   => $workers,
-            appmodule => $appmodule,
-            timeout   => $timeout,
-            environ   => $environ,
-            appdir    => "${app_dir}/webpay";
-    }
+    if($gunicorn_set) {
+        gunicorn::set {
+            $gunicorn_name:
+                porta     => "10${port}",
+                portb     => "11${port}",
+                gunicorn  => $gunicorn,
+                workers   => $workers,
+                appmodule => $appmodule,
+                timeout   => $timeout,
+                environ   => $environ,
+                appdir    => "${app_dir}/webpay";
+
+        }
+        } else {
+            gunicorn::instance {
+                $gunicorn_name:
+                    gunicorn  => $gunicorn,
+                    port      => $port,
+                    workers   => $workers,
+                    appmodule => $appmodule,
+                    timeout   => $timeout,
+                    environ   => $environ,
+                    appdir    => "${app_dir}/webpay";
+            }
+        }
 }
