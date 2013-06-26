0b7306e2b3f956c753b01c6015ac0298caa8aa55
Jeremiah Orem <oremj@oremj.com>
2013-06-25 17:34:41 -0700

    Add zamboni packages class.

---
+++ modules/marketplace/manifests/apps/zamboni/packages.pp
@@ -0,0 +1,29 @@
+# marketplace packages
+class marketplace::apps::zamboni::packages {
+    # These will probably need to be virtual someday
+    package {
+        'python-jinja2':
+            ensure => '2.5.5-1';
+
+        'python-lxml':
+            ensure => '2.2.6-1';
+
+        'python-markupsafe':
+            ensure => '0.15-1';
+
+        'python-pylibmc':
+            ensure => '1.2.3-1';
+
+        'python-simplejson':
+            ensure => '2.3.2-1';
+
+        'node-less':
+            ensure => '1.4.0-1';
+
+        'node-stylus':
+            ensure => '0.32.1-1';
+
+        'MySQL-python':
+            ensure => '1.2.3-0.3.c1.1.el6';
+    }
+}
