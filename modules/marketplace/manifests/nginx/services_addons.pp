691b939183f1c7bdf5d9631a54712179ea2fb950
Jeremiah Orem <oremj@oremj.com>
2013-07-09 15:30:53 -0700

    Add services_addons config.

---
+++ modules/marketplace/manifests/nginx/services_addons.pp
@@ -0,0 +1,14 @@
+# define services.addons nginx config
+define marketplace::nginx::services_addons(
+    $webroot
+) {
+    $server_name = $name
+    nginx::config {
+        $server_name:
+            content => template('marketplace/nginx/services_addons.conf');
+    }
+
+    nginx::logdir {
+        $server_name:;
+    }
+}
