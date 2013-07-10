0884f0e9f065ecde6d8751a413d59c524bb76f90
Jason Thomas <jason@lithiumfox.com>
2013-07-10 15:39:54 -0400

    Add base::crond

---
+++ modules/base/manifests/crond.pp
@@ -0,0 +1,14 @@
+# enable crond everywere
+class base::crond {
+
+    $cron_service = $::osfamily ? {
+        'Debian' => 'cron',
+        'RedHat' => 'crond',
+    }
+
+    service {
+        $cron_service:
+            ensure => running,
+            enable => true;
+    }
+}
