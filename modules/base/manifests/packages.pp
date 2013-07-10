ab97fa42785bdc7fb2c5f32fa33de914ab9e83e4
Jason Thomas <jason@lithiumfox.com>
2013-07-10 12:30:16 -0400

    Add base::packages

---
+++ modules/base/manifests/packages.pp
@@ -0,0 +1,20 @@
+# base packages, we install these everywhere
+class base::packages {
+    package {
+        [
+            'strace',
+            'zsh',
+            'telnet',
+            'sysstat',
+            'htop',
+            'dstat',
+            'augtool',
+            'vim-enhanced',
+            'nc',
+            'bash-completion',
+            'nano',
+        ]:
+            ensure => latest;
+
+    }
+}
