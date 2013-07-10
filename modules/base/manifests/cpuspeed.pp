809b2f12d8c5eccad3041c37f7d2f5daa29c1612
Jason Thomas <jason@lithiumfox.com>
2013-07-10 15:36:37 -0400

    Add base::cpuspeed

---
+++ modules/base/manifests/cpuspeed.pp
@@ -0,0 +1,10 @@
+# disable cpuspeed
+class base::cpuspeed {
+    if ($::osfamily == "RedHat") and ($::virtual != 'xen') {
+        service {
+            'cpuspeed':
+                ensure    => stopped,
+                enable    => false
+        }
+    }
+}
