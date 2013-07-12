65477052ed2380576ce0b45c52c5bb8e3d57e200
Jeremiah Orem <oremj@oremj.com>
2013-07-12 16:10:12 -0700

    More symlinks.

--- modules/marketplace/manifests/apps/zamboni/symlinks.pp
+++ modules/marketplace/manifests/apps/zamboni/symlinks.pp
@@ -3,23 +3,29 @@ define marketplace::apps::zamboni::symlinks(
     $netapp
 ) {
     $zamboni_webroot = "${name}/default/webroot"
+    $mkt_webroot = "${name}/mkt/webroot"
 
     file {
-        "${zamboni_webroot}/media":
+        ["${zamboni_webroot}/media",
+        "${mkt_webroot}/media"]:
             ensure => link,
             target => '../../media';
 
-        "${zamboni_webroot}/favicon.ico":
+        ["${zamboni_webroot}/favicon.ico",
+        "${mkt_webroot}/favicon.ico"]:
             ensure => link,
             target => '../../media/img/favicon.ico';
 
-        "${zamboni_webroot}/storage":
+        ["${zamboni_webroot}/storage",
+        "${mkt_webroot}/storage"]:
             ensure => directory;
 
         "${zamboni_webroot}/storage/shared_storage":
+        "${mkt_webroot}/storage/shared_storage":
             ensure => directory;
 
-        "${zamboni_webroot}/storage/files":
+        ["${zamboni_webroot}/storage/files",
+        "${mkt_webroot}/storage/files"]:
             ensure => link,
             target => "${netapp}/files";
 
@@ -31,8 +37,28 @@ define marketplace::apps::zamboni::symlinks(
             ensure => link,
             target => "${netapp}/shared_storage/ryf";
 
-        "${zamboni_webroot}/storage/shared_storage/uploads":
+        ["${zamboni_webroot}/storage/shared_storage/uploads",
+        "${mkt_webroot}/storage/shared_storage/uploads"]:
             ensure => link,
             target => "${netapp}/shared_storage/uploads";
+
+        "${mkt_webroot}/storage/shared_storage/public_keys":
+            ensure => link,
+            target => "${netapp}/shared_storage/public_keys";
+
+        "${mkt_webroot}/storage/shared_storage/inapp-image":
+            ensure => link,
+            target => "${netapp}/shared_storage/inapp-image";
+
+        "${mkt_webroot}/storage/shared_storage/product-icons":
+            ensure => link,
+            target => "${netapp}/shared_storage/product-icons";
+
+        "${mkt_webroot}/storage/shared_storage/dumped-apps":
+            ensure => directory;
+
+        "${mkt_webroot}/storage/shared_storage/dumped-apps/tarballs":
+            ensure => link,
+            target => "${netapp}/shared_storage/dumped-apps/tarballs";
     }
 }
