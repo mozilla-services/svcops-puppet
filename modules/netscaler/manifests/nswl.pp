14643eacd81d904feb1f675df3e0b4a292624180
Jeremiah Orem <oremj@oremj.com>
2013-07-13 14:12:31 -0700

    Add netscaler.

---
+++ modules/netscaler/manifests/nswl.pp
@@ -0,0 +1,19 @@
+# config for netscaler weblogger.
+class netscaler::nswl(
+    $version = 'dara_118_7',
+    $logdir = '/data/netscaler/logs',
+    $nsips = [], # list of hashes {ip => '10...', password => 'encrypted_auth_string'}
+    $filters = [] # list of hashes {name, filter, logFormat}
+){
+    package {
+        'NSweblog':
+            ensure => $version;
+    }
+
+    file {
+        $logdir:
+            ensure => directory;
+        '/etc/nswl.conf':
+            content => template('netscaler/nswl.conf');
+    }
+}
