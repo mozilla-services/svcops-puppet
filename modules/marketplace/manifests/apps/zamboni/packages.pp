c6d7eec3265efc53736e7c28e816a97d2f8261c0
Jeremiah Orem <oremj@oremj.com>
2013-07-16 17:24:44 -0700

    Use python-setproctitle.

--- modules/marketplace/manifests/apps/zamboni/packages.pp
+++ modules/marketplace/manifests/apps/zamboni/packages.pp
@@ -29,6 +29,9 @@ class marketplace::apps::zamboni::packages {
         'MySQL-python':
             ensure => '1.2.3-0.3.c1.1.el6';
 
+        'python-setproctitle':
+            ensure => present;
+
         'tracemonkey':
             ensure => '0.1-5.el6';
     }
