98488e648e95e4de581b6da7d8139db48dd90ba8
Jeremiah Orem <oremj@oremj.com>
2013-07-22 16:25:44 -0700

    Fix file names.

--- modules/netscaler/templates/nswl.logshipper.sh
+++ modules/netscaler/templates/nswl.logshipper.sh
@@ -14,6 +14,14 @@ fi
 
 RESULTLOG="/var/log/nswllogshipper/$CUR_DATE"
 
+#compress files
 find $LOG_DIR/*/ -type f -name '*.log*' ! -name '*.gz' ! -name "$CUR_DATE.log*" -print0 | xargs -0 -r -n 1 -P 4 nice -n 10 gzip
 
+#ship files
 rsync -av --ignore-existing --include='*/' --include='*.gz' --exclude='*' --remove-source-files -e "ssh -o StrictHostKeyChecking=no -i $LOG_HOST_KEY" $LOG_DIR/ $LOG_HOST_USER@$LOG_HOST:/data/stats/logs/$HOSTNAME/ &> $RESULTLOG
+
+# add a string to left over files.
+find $LOG_DIR/*/ -type f -name '*.gz' | while read f
+do
+    mv "$f" "${f%.gz}.$CUR_DATE.gz"
+done
