d421c0bdb3e55489aaca68e5792b7982641d21f4
Jeremiah Orem <oremj@oremj.com>
2013-07-22 15:01:12 -0700

    Make sure not to hose existing files.

--- modules/netscaler/templates/nswl.logshipper.sh
+++ modules/netscaler/templates/nswl.logshipper.sh
@@ -16,4 +16,4 @@ RESULTLOG="/var/log/nswllogshipper/$CUR_DATE"
 
 find $LOG_DIR/*/ -type f -name '*.log*' ! -name '*.gz' ! -name "$CUR_DATE.log*" -print0 | xargs -0 -r -n 1 -P 4 nice -n 10 gzip
 
-rsync -av --include='*/' --include='*.gz' --exclude='*' --remove-source-files -e "ssh -o StrictHostKeyChecking=no -i $LOG_HOST_KEY" $LOG_DIR/ $LOG_HOST_USER@$LOG_HOST:/data/stats/logs/$HOSTNAME/ &> $RESULTLOG
+rsync -av --ignore-existing --include='*/' --include='*.gz' --exclude='*' --remove-source-files -e "ssh -o StrictHostKeyChecking=no -i $LOG_HOST_KEY" $LOG_DIR/ $LOG_HOST_USER@$LOG_HOST:/data/stats/logs/$HOSTNAME/ &> $RESULTLOG
