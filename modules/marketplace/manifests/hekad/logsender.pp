ce8a05eb0e3e0f8d3744698914c476225a03fe50
Jeremiah Orem <oremj@oremj.com>
2013-07-26 13:49:17 -0700

    Heka logsender!

---
+++ modules/marketplace/manifests/hekad/logsender.pp
@@ -0,0 +1,12 @@
+# send nginx logs to heka master.
+class marketplace::hekad::logsender(
+    $log_host = 'localhost:5565',
+
+) {
+    hekad::instance {
+        'marketplace-logsender':
+            config  => template('marketplace/hekad/logsender.toml');
+            hekabin => '/usr/bin/hekad3';
+    }
+
+}
