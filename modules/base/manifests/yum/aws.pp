7fb4ef381b781b9e8e11bae81f420ed8e354ea35
Jeremiah Orem <oremj@oremj.com>
2013-08-15 10:40:28 -0700

    Working nginx repo.

--- modules/base/manifests/yum/aws.pp
+++ modules/base/manifests/yum/aws.pp
@@ -1,7 +1,7 @@
 # class base yum aws this conflicts with base::yum
 class base::yum::aws {
     @yumrepo { 'nginx':
-        baseurl        => 'https://mrepo.mozilla.org/mrepo/6-$basearch/RPMS.nginx',
+        baseurl        => 'http://nginx.org/packages/rhel/6/$basearch/',
         descr          => 'nginx repo',
         enabled        => 1,
         gpgcheck       => 0,
