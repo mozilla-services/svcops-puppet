7a51118f68477cb47f3a49ecc7c3f0b1b59e1b8b
Jeremiah Orem <oremj@oremj.com>
2013-08-26 15:02:57 -0700

    Move to circus (soon).

--- modules/pushgo/manifests/app.pp
+++ modules/pushgo/manifests/app.pp
@@ -4,6 +4,13 @@ class pushgo::app {
     include pushgo::config
     include supervisord::base
 
+#    circus::watcher {
+#        'pushgo':
+#            require       => File['/etc/pushgo.ini'],
+#            cmd           => '/data/pushgo.prod/www/push.mozilla.com/current/pushgo/pushgo -config=/etc/pushgo.ini',
+#            rlimit_nofile => '1000000';
+#
+#    }
     supervisord::service {
         'pushgo':
             require => File['/etc/pushgo.ini'],
