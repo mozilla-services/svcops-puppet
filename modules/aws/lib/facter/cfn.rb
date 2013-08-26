24985cddc0939c9738f5a236d2347be7c037c9a9
whd <whd@mozilla.com>
2013-08-26 12:26:39 -0700

    Try to fix borked puppet

--- modules/aws/lib/facter/cfn.rb
+++ modules/aws/lib/facter/cfn.rb
@@ -6,19 +6,19 @@ require 'json'
 
 filename = "/var/lib/cfn-init/data/metadata.json"
 if not File.exist?(filename)
-   return
-end
-
-parsed = JSON.load(File.new(filename))
-parsed.default = Hash.new
-parsed["Puppet"].each do |key, value|
-   actual_value = value
-   if value.is_a? Array
-    actual_value = value.join(',')
-   end
-   Facter.add("cfn_" + key) do
-     setcode do
-       actual_value
-     end
-   end
+  nil
+else
+  parsed = JSON.load(File.new(filename))
+  parsed.default = Hash.new
+  parsed["Puppet"].each do |key, value|
+    actual_value = value
+    if value.is_a? Array
+      actual_value = value.join(',')
+    end
+    Facter.add("cfn_" + key) do
+      setcode do
+        actual_value
+      end
+    end
+  end
 end
