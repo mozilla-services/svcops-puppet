695a96cc60f03f9664b6995021de13b56f7cd284
Jeremiah Orem <oremj@oremj.com>
2013-08-26 13:23:23 -0700

    Add supervisor to pushgo.

--- modules/pushgo/manifests/app.pp
+++ modules/pushgo/manifests/app.pp
@@ -1,4 +1,14 @@
+# pushgo app
 class pushgo::app {
     include pushgo::sysctl
     include pushgo::config
+    include supervisord::base
+
+    supervisord::service {
+        'pushgo':
+            require => File['/etc/pushgo.ini'],
+            command => '/opt/pushgo/pushgo -config=/etc/pushgo.ini',
+            app_dir => '/opt/pushgo',
+            user    => 'root';
+    }
 }
