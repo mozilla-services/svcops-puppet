ef45e5780133dc227641eb4458d62b86ac473855
Jeremiah Orem <oremj@oremj.com>
2013-09-05 16:03:26 -0700

    Add admin cluster.

--- modules/pushgo/manifests/admin.pp
+++ modules/pushgo/manifests/admin.pp
@@ -1,22 +1,10 @@
 # pushgo admin server.
 class pushgo::admin(
-    $cluster='pushgo.prod',
-    $s3_bucket='bucket'
+    $clusters={'pushgo.prod' => {s3_bucket => 'bucket'}}
 ){
-    $cluster_src = "/data/${cluster}/src"
     package {
         'libmemcached-devel':
             ensure => '1.0.16-1.el6';
     }
-
-    git::clone {
-        "${cluster_src}/push.mozilla.com/pushgo":
-            repo => 'https://github.com/jrconlin/pushgo.git';
-    }
-
-    file {
-        "${cluster_src}/push.mozilla.com/fabfile.py":
-            require => Git::Clone["${cluster_src}/push.mozilla.com/pushgo"],
-            content => template('pushgo/admin/fabfile.py');
-    }
+    create_resources(pushgo::admin_cluster, $clusters)
 }
---
+++ modules/pushgo/manifests/admin_cluster.pp
@@ -0,0 +1,17 @@
+# add a cluster to the admin server
+define pushgo::admin_cluster(
+    $s3_bucket
+) {
+    $cluster = $name
+    $cluster_src = "/data/${cluster}/src"
+    git::clone {
+        "${cluster_src}/push.mozilla.com/pushgo":
+            repo => 'https://github.com/mozilla-services/pushgo.git';
+    }
+
+    file {
+        "${cluster_src}/push.mozilla.com/fabfile.py":
+            require => Git::Clone["${cluster_src}/push.mozilla.com/pushgo"],
+            content => template('pushgo/admin/fabfile.py');
+    }
+}
