967747d461a2b834187f7df5842404de68654bc2
Jeremiah Orem <oremj@oremj.com>
2013-09-16 16:27:08 -0700

    Add monolith uwsgi and settings.

---
+++ modules/marketplace/manifests/apps/monolith.pp
@@ -0,0 +1,19 @@
+# monolith
+define marketplace::apps::monolith(
+    $project_dir,
+    $workers = 4,
+    $port = '12000',
+    $user = 'mkt_prod_monolith'
+) {
+    $uwsgi_name = $name
+
+    uwsgi::isntance {
+        $uwsgi_name:
+            app_dir   => "${project_dir}/monolith",
+            appmodule => "runserver:application",
+            port      => $port,
+            home      => "${project_dir}/venv",
+            user      => $user,
+            workers   => $workers;
+    }
+}
