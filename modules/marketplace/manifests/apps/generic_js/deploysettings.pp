4d79b08fd2887898316ee4dd84fc11c4b08c7af4
Jeremiah Orem <oremj@oremj.com>
2013-09-17 16:57:53 -0700

    Add marketplace stats.

---
+++ modules/marketplace/manifests/apps/generic_js/deploysettings.pp
@@ -0,0 +1,14 @@
+# $name is the location of rocketfuel
+define marketplace::apps::generic_js::deploysettings(
+    $cluster,
+    $domain,
+    $env,
+    $ssh_key
+) {
+    $app_dir = $name
+
+    file {
+        "${app_dir}/deploysettings.py":
+            content => template('marketplace/apps/generic_js/deploysettings.py');
+    }
+}
---
+++ modules/marketplace/templates/apps/generic_js/deploysettings.py
@@ -0,0 +1,4 @@
+CLUSTER = '<%= cluster %>'
+DOMAIN = '<%= domain %>'
+ENV = '<%= env %>'
+SSH_KEY = '<%= ssh_key %>'
