e2ce862925fbfa94996f87bd29d30b53713f00b1
Jeremiah Orem <oremj@oremj.com>
2013-09-16 17:07:29 -0700

    Remove notifications.

--- modules/marketplace/manifests/apps/monolith/web.pp
+++ modules/marketplace/manifests/apps/monolith/web.pp
@@ -3,8 +3,7 @@ class marketplace::apps::monolith::web(
     $instances = {}
 ) {
 
-    notify { "${instances}" }
-    notify { "${nginx}" }
+    include nginx
     create_resources(marketplace::apps::monolith, $instances)
     create_resources(marketplace::nginx::monolith, $nginx)
 
