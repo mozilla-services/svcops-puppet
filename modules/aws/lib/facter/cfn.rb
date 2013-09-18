701ad71a09ef58dea48fbfa84fe9f49bfbd5a251
Jeremiah Orem <oremj@oremj.com>
2013-09-18 16:41:09 -0700

    Add exception handling to cfn fact.

--- modules/aws/lib/facter/cfn.rb
+++ modules/aws/lib/facter/cfn.rb
@@ -1,23 +1,26 @@
 # cfn.rb
 # Turns cloudformation metadata in to facts.
+begin
+  require 'json'
 
-require 'json'
-
-filename = "/var/lib/cfn-init/data/metadata.json"
-if not File.exist?(filename)
-  nil
-else
-  parsed = JSON.load(File.new(filename))
-  parsed.default = Hash.new
-  parsed["Puppet"].each do |key, value|
-    actual_value = value
-    if value.is_a? Array
-      actual_value = value.join(',')
-    end
-    Facter.add("cfn_" + key) do
-      setcode do
-        actual_value
+  filename = "/var/lib/cfn-init/data/metadata.json"
+  if not File.exist?(filename)
+    nil
+  else
+    parsed = JSON.load(File.new(filename))
+    parsed.default = Hash.new
+    parsed["Puppet"].each do |key, value|
+      actual_value = value
+      if value.is_a? Array
+        actual_value = value.join(',')
+      end
+      Facter.add("cfn_" + key) do
+        setcode do
+          actual_value
+        end
       end
     end
   end
+rescue LoadError
+  nil
 end
