acfcae628e381af12833401382fa771c7ff64ce2
Jeremiah Orem <oremj@oremj.com>
2013-09-25 08:33:12 -0700

    Add deploytools package deps.

--- modules/fabdeploytools/manifests/deploytools.pp
+++ modules/fabdeploytools/manifests/deploytools.pp
@@ -1,9 +1,20 @@
 # set up for deploytools
-class fabdeploytools::deploytools {
+class fabdeploytools::deploytools(
+    $fabric_version = 'present',
+    $fpm_version = 'present',
+    $fabdeploytools_version = 'present'
+){
     package {
         'prelink':
             ensure => absent;
+        'python-fabric':
+            ensure => $fabric_version;
+        'rubygem-fpm':
+            ensure => $fpm_version;
+        'python-fabdeploytools':
+            ensure => $fabdeploytools_version;
     }
+
     file {
         '/etc/deploytools':
             ensure  => directory,
