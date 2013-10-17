c48a892d8a36132fd4a9aa34c3a1f7337d4ac598
Jason Thomas <jason@lithiumfox.com>
2013-10-17 11:00:24 -0400

    Add tuned module.

---
+++ modules/tuned/manifests/init.pp
@@ -0,0 +1,28 @@
+# install and configure tuned
+class tuned (
+    $profile = 'throughput-performance'
+){
+
+    $lock = '/root/.tuned'
+
+    package {
+        'tuned':
+            ensure => present;
+    }
+
+    exec {
+        'enable-tuned-profile':
+            command   => "/usr/bin/tuned-adm profile ${profile} && touch ${lock}",
+            require   => Package['tuned'],
+            creates   => $lock;
+    }
+
+    service {
+        'tuned':
+            ensure    => running,
+            enable    => true,
+            hasstatus => true,
+            require   => Exec['enable-tuned-profile'];
+    }
+
+}
