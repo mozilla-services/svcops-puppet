7428d57b5ca151869b8187130a5aa6558fe8c68f
Jason Thomas <jason@lithiumfox.com>
2013-10-17 16:12:09 -0400

    Add marketplace-stats admin definition.

---
+++ modules/marketplace/manifests/apps/marketplace_stats/admin.pp
@@ -0,0 +1,6 @@
+# admin instance class
+class marketplace::apps::marketplace_stats::admin(
+    $instances = {},
+) {
+    create_resources(marketplace::apps::marketplace_stats::admin_instance, $instances)
+}
---
+++ modules/marketplace/manifests/apps/marketplace_stats/admin_instance.pp
@@ -0,0 +1,23 @@
+# define admin instance for marketplace stats
+define marketplace::apps::marketplace_stats::admin_instance(
+    $cluster,
+    $domain,
+    $env,
+    $ssh_key,
+    $dreadnot_name,
+    $dreadnot_instance,
+) {
+    $project_dir = $name
+    git::clone {
+        "${project_dir}/marketplace-stats":
+            repo => 'https://github.com/mozilla/marketplace-stats.git';
+
+    }
+    dreadnot::stack {
+        $dreadnot_name:
+            instance_name => $dreadnot_instance,
+            project_dir   => "${project_dir}/marketplace-stats",
+            github_url    => 'https://github.com/mozilla/marketplace-stats',
+            git_url       => 'git://github.com/mozilla/marketplace-stats.git';
+    }
+}
