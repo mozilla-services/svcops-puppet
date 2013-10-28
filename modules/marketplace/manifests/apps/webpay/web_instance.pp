5f78adfa33876bef11641968f171ad6a5843dfe1
Jeremiah Orem <oremj@oremj.com>
2013-10-28 15:10:10 -0700

    Sync webpay changes.

---
+++ modules/marketplace/manifests/apps/webpay/web_instance.pp
@@ -0,0 +1,34 @@
+# define zamboni instance.
+define marketplace::apps::webpay::web_instance(
+    $worker_name,
+    $port,
+    $app_dir,
+    $appmodule = 'wsgi.playdoh:application',
+    $workers = 12,
+    $timeout = '90',
+    $environ = '',
+    $newrelic_license_key = '',
+    $user = 'mkt_prod_webpay'
+) {
+    $app_name = $name
+    $gunicorn = "${app_dir}/venv/bin/gunicorn"
+
+    if $newrelic_license_key {
+        marketplace::newrelic::python {
+            $app_name:
+                before      => Uwsgi::Instance[$worker_name],
+                license_key => $newrelic_license_key;
+        }
+    }
+
+    uwsgi::instance {
+        $worker_name:
+            app_dir   => "${app_dir}/webpay",
+            appmodule => $appmodule,
+            port      => "12${port}",
+            home      => "${app_dir}/venv",
+            user      => $user,
+            workers   => $workers,
+            environ   => $environ;
+    }
+}
