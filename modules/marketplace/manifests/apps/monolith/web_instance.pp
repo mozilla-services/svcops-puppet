ed74e7ab107df780eba309bf93252bd335a72df0
Jason Thomas <jason@lithiumfox.com>
2013-10-29 10:46:03 -0400

    Update monolith web.

--- modules/marketplace/manifests/apps/monolith/web_instance.pp
+++ modules/marketplace/manifests/apps/monolith/web_instance.pp
@@ -1,30 +1,30 @@
 # describe monolith on a webserver.
 define marketplace::apps::monolith::web_instance(
     $server_names, # a list of names
-    $worker_name,
-    $project_dir,
-    $workers = 4,
-    $port = '12000',
+    $elasticsearch_endpoint,
+    $es_index_prefix = '',
     $user = 'mkt_prod_monolith'
 ) {
     $config_name = $name
+
+    $es_location = $es_index_prefix ? {
+        ''      => 'time',
+        default => "${es_index_prefix}-time"
+    }
+
     nginx::config {
         $config_name:
-            require => Uwsgi::Instance[$worker_name],
+            require => Nginx::Upstream['monolith-es'],
             content => template('marketplace/apps/monolith/web/monolith.conf');
+    }
+
+    nginx::upstream {
+        'monolith-es':
+            upstream_port => '9200',
+            upstream_host => $elasticsearch_endpoint
     }
 
     nginx::logdir {
         $config_name:;
-    }
-
-    uwsgi::instance {
-        $worker_name:
-            app_dir   => "${project_dir}/monolith",
-            appmodule => 'runserver:application',
-            port      => $port,
-            home      => "${project_dir}/venv",
-            user      => $user,
-            workers   => $workers;
     }
 }
