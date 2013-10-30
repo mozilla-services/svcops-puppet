8e70d2885418befafb19920dae331f584e117b28
Jeremiah Orem <oremj@oremj.com>
2013-10-30 14:40:06 -0700

    Add dev/stage solitude.

---
+++ modules/marketplace/manifests/apps/solitude/web_instance.pp
@@ -0,0 +1,50 @@
+# solitude web instance
+define marketplace::apps::solitude::web_instance(
+    $project_dir,
+    $port,
+    $server_names, # ['payments.mozilla.org']
+    $workers = 12,
+    $worker_name = 'payments',
+    $user = 'sol_prod',
+    $settings_module = 'solitude.settings',
+    $appmodule = 'wsgi.playdoh:application',
+    $is_proxy = false,
+    $newrelic_license_key = ''
+) {
+    $config_name = $name
+
+    if $is_proxy {
+        $environ = ',SOLITUDE_PROXY=\'enabled\''
+    } else {
+        $environ = ''
+    }
+
+    if $newrelic_license_key {
+        marketplace::newrelic::python {
+            $config_name:
+                before      => Uwsgi::Instance[$worker_name],
+                license_key => $newrelic_license_key;
+        }
+    }
+
+    uwsgi::instance {
+        $worker_name:
+            app_dir   => "${project_dir}/solitude",
+            appmodule => $appmodule,
+            port      => $port,
+            home      => "${project_dir}/venv",
+            user      => $user,
+            workers   => $workers,
+            environ   => "DJANGO_SETTINGS_MODULE=${settings_module}${environ}";
+    }
+
+    nginx::config {
+        $config_name:
+            require => Uwsgi::Instance[$worker_name],
+            content => template('marketplace/apps/solitude/web/nginx.conf');
+    }
+
+    nginx::logdir {
+        $config_name:;
+    }
+}
