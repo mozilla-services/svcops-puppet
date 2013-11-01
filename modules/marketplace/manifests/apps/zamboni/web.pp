697c0ba2e0e90f208afc2a990dee9b714663d0a2
Jason Thomas <jason@lithiumfox.com>
2013-11-01 17:12:46 -0400

    Fix fat finger.

--- modules/marketplace/manifests/apps/zamboni/web.pp
+++ modules/marketplace/manifests/apps/zamboni/web.pp
@@ -1,5 +1,5 @@
 # marketplace web class.
-class marketplace::apps::monolith::zamboni::web(
+class marketplace::apps::zamboni::web(
     $instances = {}
 ) {
     create_resources(marketplace::apps::zamboni, $instances)
