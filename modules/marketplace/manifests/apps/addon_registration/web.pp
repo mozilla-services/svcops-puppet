3cf0cada4979972ffaeaaa18be5f0074db3e0d21
Jason Thomas <jason@lithiumfox.com>
2013-11-20 13:26:56 -0500

    Add web configuration for addon_registration.

---
+++ modules/marketplace/manifests/apps/addon_registration/web.pp
@@ -0,0 +1,6 @@
+# marketplace addon_registration web class.
+class marketplace::apps::addon_registration::web(
+    $instances = {}
+) {
+    create_resources(marketplace::apps::addon_registration::web_instance, $instances)
+}
