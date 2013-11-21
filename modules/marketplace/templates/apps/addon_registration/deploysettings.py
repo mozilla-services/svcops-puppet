329b1c4c694c5f24489e5fe542f36922a6b06747
Jason Thomas <jason@lithiumfox.com>
2013-11-21 14:46:47 -0500

    Add SCL_NAME to addon registration deploysettings.

--- modules/marketplace/templates/apps/addon_registration/deploysettings.py
+++ modules/marketplace/templates/apps/addon_registration/deploysettings.py
@@ -6,3 +6,4 @@ PYREPO = '<%= @pyrepo %>'
 UWSGI = filter(None, '<%= @uwsgi %>'.split(';'))
 CELERY_SERVICE = '<%= @celery_service %>'
 UPDATE_REF = <% if update_ref %>'<%= @update_ref %>'<% else %>None<% end %>
+SCL_NAME = 'python27'
