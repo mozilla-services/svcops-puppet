6217e8d6ae46f8921517c62b52573db8d2d9f4a6
Jason Thomas <jason@lithiumfox.com>
2013-11-26 13:36:36 -0500

    Use environment scl environment variable instead of scl enable command.

--- modules/marketplace/manifests/apps/addon_registration/celery_instance.pp
+++ modules/marketplace/manifests/apps/addon_registration/celery_instance.pp
@@ -10,8 +10,8 @@ define marketplace::apps::addon_registration::celery_instance(
 
     $addon_registration_dir = "${app_dir}/current/addon_registration"
     $addon_registration_python = "${app_dir}/current/venv/bin/python"
-    $environ = "CONFIG=${addon_registration_dir}/production.ini"
-    $command = "/bin/bash -c \"source /opt/rh/${scl}/enable; exec ${addon_registration_python} ${addon_registration_dir}/addonreg/worker.py worker\""
+    $environ = "CONFIG=${addon_registration_dir}/production.ini,LD_LIBRARY_PATH=/opt/rh/${scl}/root/usr/lib64,PATH=/opt/rh/${scl}/root/usr/bin:/sbin:/usr/sbin:/bin:/usr/bin"
+    $command = "${addon_registration_python} ${addon_registration_dir}/addonreg/worker.py worker"
 
     Celery::Service {
         command => $command,
