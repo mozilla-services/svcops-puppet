44193d85be297100a376183cd5c9793d39febdc2
Jason Thomas <jason@lithiumfox.com>
2013-11-27 16:19:43 -0500

    Manage fireplace media dir in zamboni dir.

---
+++ modules/marketplace/manifests/apps/zamboni/symlinks/fireplace.pp
@@ -0,0 +1,12 @@
+# manages fireplace media symlink
+define marketplace::apps::zamboni::symlinks::fireplace(
+    $fireplace_dir
+) {
+    $zamboni_media = "${name}/media"
+
+    file {
+        "${zamboni_media}/fireplace":
+            ensure => link,
+            target => "${fireplace_dir}/fireplace/hearth/media"
+    }
+}
