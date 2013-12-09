109436a3be12817ae83d5eebf1d6a6d13ed54e2f
Jason Thomas <jason@lithiumfox.com>
2013-12-09 17:54:05 -0500

    Add apache::sslvserverproxy

---
+++ modules/apache/manifests/sslvserverproxy.pp
@@ -0,0 +1,12 @@
+# set up a ssl vserver with a proxy.
+define apache::sslvserverproxy(
+    $proxyto,
+    $ssl_key,
+    $ssl_certificate,
+) {
+    $server_name = $name
+    apache::config {
+        $server_name:
+            content => template('apache/sslvserverproxy.conf');
+    }
+}
