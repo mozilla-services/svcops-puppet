3d7468973adbd15e9ec5788e44f84cfc3d8c98c9
Jeremiah Orem <oremj@oremj.com>
2013-12-10 11:54:44 -0800

    Upgrade heka to 0.4.1.

--- modules/hekad/manifests/init.pp
+++ modules/hekad/manifests/init.pp
@@ -1,33 +1,25 @@
+# hekad
 class hekad(
     $log_dir = '/var/log/hekad',
     $udp_listen_address = '127.0.0.1:5565',
     $cef_address = '127.0.0.1:5565',
     $statsd_address = '127.0.0.1:8125',
-    $version = '0.1.1-4'
+    $version = '0.4.1-0'
 ){
     package {
         'hekad':
             ensure => $version,
             notify => Service['hekad'];
     }
+
     file {
         $log_dir:
             ensure => directory;
 
-        '/etc/hekad.json':
-            require => Package['hekad'],
-            notify  => Service['hekad'],
-            content => template('hekad/hekad.json.erb')
     }
 
-    supervisord::service {
+    hekad::instance {
         'hekad':
-            user    => 'root',
-            command => '/usr/bin/hekad',
-            app_dir => '/usr/bin',
-            require => [
-                File['/etc/hekad.json'],
-                File[$log_dir],
-            ];
+            config => template('hekad/hekad.toml.erb');
     }
 }
