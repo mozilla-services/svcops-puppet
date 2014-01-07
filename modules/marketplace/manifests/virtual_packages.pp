d9da1238ae2b0ac51b39a77e4e013014f6856d32
Jason Thomas <jason@lithiumfox.com>
2014-01-07 13:03:00 -0500

    Update nodejs package.

--- modules/marketplace/manifests/virtual_packages.pp
+++ modules/marketplace/manifests/virtual_packages.pp
@@ -41,7 +41,7 @@ class marketplace::virtual_packages {
             ensure => '2.3.2-1';
 
         'nodejs':
-            ensure => '0.10.22-1.el6';
+            ensure => '0.10.24-1.el6';
 
         'node-clean-css':
             ensure => '0.10.1-2';
