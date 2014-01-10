2080222ad039880b98cd5e7bcb65faa7d890b1fd
Jeremiah Orem <oremj@oremj.com>
2014-01-10 09:32:22 -0800

    twemproxy: manage /etc/nutcracker.

--- modules/twemproxy/manifests/pool.pp
+++ modules/twemproxy/manifests/pool.pp
@@ -1,14 +1,15 @@
+# add a pool to a twemproxy config.
 define twemproxy::pool(
-    $listen,
-    $servers,
     $config_file,
+    $servers = [],
+    $listen = '127.0.0.1:22122',
     $twemproxy_hash = 'fnv1a_64',
     $distribution = 'ketama',
     $server_timeout = '1000',
     $listen_backlog = '512',
-    $redis = 'false',
-    $preconnect = 'false',
-    $auto_eject_hosts = 'false',
+    $redis = false,
+    $preconnect = false,
+    $auto_eject_hosts = false,
     $server_retry_timeout = '30000',
     $server_failure_limit = '2',
     $server_connections = '1'
