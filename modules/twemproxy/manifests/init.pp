cdf96c6c260ad639779c704616eda3d374257307
Jeremiah Orem <oremj@oremj.com>
2014-01-10 09:32:22 -0800

    twemproxy: manage /etc/nutcracker.

--- modules/twemproxy/manifests/init.pp
+++ modules/twemproxy/manifests/init.pp
@@ -1,10 +1,18 @@
+# install twemproxy and create directories.
 class twemproxy(
-    $servers,
-    $version = "0.3.0-1"
+    $servers = {},
+    $version = '0.3.0-1'
 ){
     package  {
         'nutcracker':
             ensure => $version;
+    }
+
+    file {
+        '/etc/nutcracker':
+            ensure  => directory,
+            purge   => true,
+            recurse => true;
     }
 
     create_resources(twemproxy::server, $servers)
