bb8b028f92b80c9e7bf2b6dad77965f25e5c2684
Jeremiah Orem <oremj@oremj.com>
2014-01-10 09:29:12 -0800

    twemproxy::server: add supervisor service.

--- modules/twemproxy/manifests/server.pp
+++ modules/twemproxy/manifests/server.pp
@@ -1,12 +1,15 @@
+# start a twemproxy server.
 define twemproxy::server(
     $pools
 ) {
     include concat::setup
 
     $server_name = $name
+    $service_name = "twemproxy-${server_name}"
     $config_file = "/etc/nutcracker/${server_name}.yml"
     $pool_defaults = {
         'config_file' => $config_file,
+        'before'      => Supervisord::Service[$service_name]
     }
 
     concat {
@@ -17,4 +20,11 @@ define twemproxy::server(
     }
 
     create_resources(twemproxy::pool, $pools, $pool_defaults)
+
+    supervisord::service {
+        $service_name:
+            command => "/usr/sbin/nutcracker -c \"${config_file}\"",
+            app_dir => '/tmp',
+            user    => 'nobody';
+    }
 }
