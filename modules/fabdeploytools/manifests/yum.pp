8c5552bffbb8343d0ab728fac504f9e246921fec
Jason Thomas <jason@lithiumfox.com>
2014-01-17 16:17:43 -0500

    Update deploytools yum settings.

--- modules/fabdeploytools/manifests/yum.pp
+++ modules/fabdeploytools/manifests/yum.pp
@@ -3,11 +3,12 @@ class fabdeploytools::yum(
     $package_server
 ) {
     yumrepo { 'deploytools':
-        baseurl        => $package_server,
-        descr          => 'Deploytools Repo',
-        enabled        => 1,
-        priority       => 2,
-        gpgcheck       => 0,
-        failovermethod => priority,
+        baseurl         => $package_server,
+        descr           => 'Deploytools Repo',
+        enabled         => 1,
+        metadata_expire => '30',
+        priority        => 1,
+        gpgcheck        => 0,
+        failovermethod  => priority,
     }
 }
