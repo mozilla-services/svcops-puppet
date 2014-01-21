beb3ec20464584c51d77a764af8b9a8840321034
Jason Thomas <jason@lithiumfox.com>
2014-01-21 16:34:24 -0500

    Update hekad configuration.

--- modules/hekad/manifests/init.pp
+++ modules/hekad/manifests/init.pp
@@ -4,6 +4,8 @@ class hekad(
     $udp_listen_address = '127.0.0.1:5565',
     $cef_address = '127.0.0.1:5565',
     $statsd_address = '127.0.0.1:8125',
+    $carbon_output_address = '127.0.0.1:2003',
+    $log_output = false,
     $version = '0.4.3-1'
 ){
     package {
