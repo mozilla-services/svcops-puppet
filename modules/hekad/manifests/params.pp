5a571fac422052148a5a2576b0016431f533e9c8
Wesley Dawson <whd@mozilla.com>
2014-01-21 22:02:55 -0800

    Fix hekad module for 0.4* series hekas

--- modules/hekad/manifests/params.pp
+++ modules/hekad/manifests/params.pp
@@ -1,13 +1,5 @@
 # heka params
 class hekad::params {
     $config_dir = '/etc/heka.d'
-
-    file {
-        $config_dir:
-            ensure  => directory,
-            purge   => true,
-            recurse => true;
-        ['/var/run/hekad', '/var/run/hekad/seekjournals']:
-            ensure => directory;
-    }
+    $version = '0.4.3-1'
 }
