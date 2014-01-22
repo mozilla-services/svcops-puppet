a7f742a0f8be7ef518f2ecffd9072783475a7501
Jason Thomas <jason@lithiumfox.com>
2014-01-22 11:18:25 -0800

    Revert "Use template always."
    This reverts commit b7af6006a2a5a33235806ad5a3ce584dd1e30f33.

--- modules/hekad/manifests/instance.pp
+++ modules/hekad/manifests/instance.pp
@@ -9,7 +9,7 @@ define hekad::instance(
     file {
         "${hekad::config_dir}/${heka_name}.toml":
             require => Class['hekad'],
-            content => template($config);
+            content => $config;
     }
 
     supervisord::service {
