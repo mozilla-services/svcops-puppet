34e78a06acf9b341ee7b0e6bcef5e9d3541d8de1
Jason Thomas <jason@lithiumfox.com>
2014-01-27 18:39:24 -0500

    Add npm_mirror.

---
+++ modules/npm_mirror/manifests/init.pp
@@ -0,0 +1,8 @@
+class npm_mirror::server(
+  $port = '8000',
+  $npm_location = '/data/npm'
+){
+
+  include npm_mirror
+
+}
---
+++ modules/npm_mirror/manifests/sync.pp
@@ -0,0 +1,5 @@
+class npm_mirror::sync(
+  $npm_location = '/data/npmrepo'
+){
+  include npm_mirror
+}
