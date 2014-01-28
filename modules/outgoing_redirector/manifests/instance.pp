76014824a218627ae30cd917fd92c98a4fc530c8
Jeremiah Orem <oremj@oremj.com>
2014-01-28 15:13:48 -0800

    outgoing_redirector: initial commit.

---
+++ modules/outgoing_redirector/manifests/instance.pp
@@ -0,0 +1,13 @@
+# set up instance of the outgoing redirector.
+define outgoing_redirector::instance(
+    $addr,
+    $secret_key,
+) {
+    $inst_name = $name
+    supervisord::service { "outgoing-${inst_name}":
+        command => "/usr/bin/outgoing-redirector -addr \"${addr}\" -key \"${secret_key}\"",
+        app_dir => '/tmp',
+        user    => 'nobody',
+        require => Package['outgoing-redirector']
+    }
+}
