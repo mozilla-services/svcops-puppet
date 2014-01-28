0361613a2dea24a542bd8be04df365ff902aa509
Jason Thomas <jason@lithiumfox.com>
2014-01-27 17:16:41 -0800

    Fix syntax.

--- modules/npm_mirror/manifests/init.pp
+++ modules/npm_mirror/manifests/init.pp
@@ -1,6 +1,6 @@
 # npm_mirror
 class npm_mirror(
-    $npm_mirror_dir = '/opt/npm_mirror'
+    $npm_mirror_dir = '/opt/npm_mirror',
     $giturl = 'https://github.com/mozilla-b2g/npm-mirror.git'
 ){
 
