91b703e9c0846243a273a68138d69506d1fae003
Jeremiah Orem <oremj@oremj.com>
2014-01-28 15:18:06 -0800

    outgoing_redirector: include init class.

--- modules/outgoing_redirector/manifests/instance.pp
+++ modules/outgoing_redirector/manifests/instance.pp
@@ -3,6 +3,8 @@ define outgoing_redirector::instance(
     $addr,
     $secret_key,
 ) {
+    include outgoing_redirector
+
     $inst_name = $name
     supervisord::service { "outgoing-${inst_name}":
         command => "/usr/bin/outgoing-redirector -addr \"${addr}\" -key \"${secret_key}\"",
