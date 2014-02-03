0e8ef59840565a5114f1bacb212e7cf2090a25cd
Jason Thomas <jason@lithiumfox.com>
2014-02-03 13:56:29 -0500

    Add nginx::sslserverproxy.

---
+++ modules/nginx/manifests/sslserverproxy.pp
@@ -0,0 +1,12 @@
+# create a location / in a vhost that proxies somewhere else.
+define nginx::sslserverproxy(
+    $proxyto # http://testhost
+    $ssl_key,
+    $ssl_certificate,
+) {
+    $server_name = $name
+    nginx::config {
+        $server_name:
+            content => template('nginx/sslserverproxy.conf');
+    }
+}
