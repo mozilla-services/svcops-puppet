afac21019bd16db87d5ab57766c0c874a4a8a415
Jeremiah Orem <oremj@oremj.com>
2014-02-11 16:41:25 -0800

    retab(sentry)

--- modules/sentry/manifests/init.pp
+++ modules/sentry/manifests/init.pp
@@ -1,15 +1,15 @@
 # base sentry class
 class sentry {
-    package {
-        'sentry':
-            ensure => '6.4.4-1';
-        'python-eventlet':
-            ensure => 'latest';
-    }
-    file {
-        '/etc/sentry.d':
-            ensure  => directory,
-            recurse => true,
-            purge   => true;
-    }
+  package {
+    'sentry':
+      ensure => '6.4.4-1';
+    'python-eventlet':
+      ensure => 'latest';
+  }
+  file {
+    '/etc/sentry.d':
+      ensure  => 'directory',
+      recurse => true,
+      purge   => true;
+  }
 }
