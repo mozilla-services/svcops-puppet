424f53ac6288b6f95284be7fabc6cf2c3793463d
Jeremiah Orem <oremj@oremj.com>
2014-02-11 16:41:25 -0800

    retab(sentry)

--- modules/sentry/manifests/service.pp
+++ modules/sentry/manifests/service.pp
@@ -1,36 +1,35 @@
-#define sentry service
+# define sentry service
 define sentry::service(
-    $db_name,
-    $db_pass,
-    $sentry_key,
-    $sentry_port,
-    $server_email,
-    $email_host
+  $db_name,
+  $db_pass,
+  $sentry_key,
+  $sentry_port,
+  $server_email,
+  $email_host
 ) {
-    $sentry_name = $name
+  $sentry_name = $name
 
-    include sentry
+  include sentry
 
-    file {
-        "/etc/sentry.d/${sentry_name}.py":
-            content => template('sentry/sentry.conf');
-    }
+  file {
+    "/etc/sentry.d/${sentry_name}.py":
+      content => template('sentry/sentry.conf');
+  }
 
-    file {
-        "/etc/httpd/conf.d/${sentry_name}.conf":
-            notify  => Service['httpd'],
-            content => template('sentry/apache.conf');
-    }
+  file {
+    "/etc/httpd/conf.d/${sentry_name}.conf":
+      notify  => Service['httpd'],
+      content => template('sentry/apache.conf');
+  }
 
-    supervisord::service {
-        "sentry-${sentry_name}-http":
-            command => "/opt/sentry/bin/sentry --config=/etc/sentry.d/${sentry_name}.py start http",
-            app_dir => '/tmp',
-            user    => 'apache';
-        "sentry-${sentry_name}-udp":
-            command => "/opt/sentry/bin/sentry --config=/etc/sentry.d/${sentry_name}.py start udp",
-            app_dir => '/tmp',
-            user    => 'apache';
-
-    }
+  supervisord::service {
+    "sentry-${sentry_name}-http":
+      command => "/opt/sentry/bin/sentry --config=/etc/sentry.d/${sentry_name}.py start http",
+      app_dir => '/tmp',
+      user    => 'apache';
+    "sentry-${sentry_name}-udp":
+      command => "/opt/sentry/bin/sentry --config=/etc/sentry.d/${sentry_name}.py start udp",
+      app_dir => '/tmp',
+      user    => 'apache';
+  }
 }
