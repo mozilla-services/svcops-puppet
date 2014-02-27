dfff23fd4b280df1c25be8aae594fa9072cf5e44
Jeremiah Orem <oremj@oremj.com>
2014-02-27 14:47:39 -0800

    feat(olympia): add admin definition

---
+++ modules/marketplace/manifests/apps/olympia/admin_instance.pp
@@ -0,0 +1,80 @@
+# Creates zamboni settings file.
+define marketplace::apps::olympia::admin_instance(
+  $api_access_secret_key,
+  $broker_url,
+  $builder_secret_key,
+  $caches_default_location,
+  $databases_default_url,
+  $databases_slave_url,
+  $dreadnot_name,
+  $dreadnot_instance,
+  $email_blacklist,
+  $email_host,
+  $es_hosts,
+  $google_analytics_credentials,
+  $google_api_credentials,
+  $graphite_host,
+  $graphite_port,
+  $graphite_prefix,
+  $heka_conf_sender_host,
+  $heka_conf_sender_port,
+  $netapp_storage_root,
+  $recaptcha_private_key,
+  $recaptcha_public_key,
+  $redirect_secret_key,
+  $redis_backends_cache,
+  $redis_backends_cache_slave,
+  $redis_backends_master,
+  $redis_backends_slave,
+  $responsys_id,
+  $services_database_url,
+  $statsd_host,
+  $statsd_port,
+  $statsd_prefix,
+  $addons_paypal_app_id,
+  $addons_paypal_embedded_auth_user,
+  $addons_paypal_embedded_password,
+  $addons_paypal_embedded_signature,
+  $addons_responsys_id,
+  $addons_secret_key,
+  $addons_sentry_dsn,
+  $aws_access_key_id = '',
+  $aws_secret_access_key = '',
+  $aws_storage_bucket_name = '',
+  $secret_key = '',
+  $addons_domain = undef,
+  $addons_paypal_cgi_auth_password = '',
+  $addons_paypal_cgi_auth_signature = '',
+  $addons_paypal_cgi_auth_user = '',
+  $addons_paypal_chains_email = '',
+  $addons_paypal_email = '',
+  $addons_paypal_embedded_auth_password = '',
+  $addons_paypal_embedded_auth_signature = '',
+  $addons_static_url = undef,
+  $addons_webapps_receipt_key = '',
+  $env = 'dev',
+) {
+  $project_dir = $name
+  $app_dir = "${project_dir}/olympia"
+
+  git::clone { $app_dir:
+    repo => 'https://github.com/mozilla/olympia.git',
+  }
+
+  file {
+    "${app_dir}/sites/${env}/private_base.py":
+      require => Git::Clone[$app_dir],
+      content => template('marketplace/apps/olympia/settings/private_base.py');
+
+    "${app_dir}/sites/${env}/private_addons.py":
+      require => Git::Clone[$app_dir],
+      content => template('marketplace/apps/olympia/settings/private_addons.py');
+  }
+
+  dreadnot::stack { $dreadnot_name:
+    instance_name => $dreadnot_instance,
+    project_dir   => $app_dir,
+    github_url    => 'https://github.com/mozilla/olympia',
+    git_url       => 'https://github.com/mozilla/olympia.git',
+  }
+}
---
+++ modules/marketplace/templates/apps/olympia/settings/private_addons.py
@@ -0,0 +1,21 @@
+PAYPAL_APP_ID = '<%= addons_paypal_app_id %>'
+PAYPAL_CGI_AUTH_PASSWORD = '<%= addons_paypal_cgi_auth_password %>'
+PAYPAL_CGI_AUTH_SIGNATURE = '<%= addons_paypal_cgi_auth_signature %>'
+PAYPAL_CGI_AUTH_USER = '<%= addons_paypal_cgi_auth_user %>'
+PAYPAL_CHAINS_EMAIL = '<%= addons_paypal_chains_email %>'
+PAYPAL_EMAIL = '<%= addons_paypal_email %>'
+PAYPAL_EMBEDDED_AUTH_PASSWORD = '<%= addons_paypal_embedded_auth_password %>'
+PAYPAL_EMBEDDED_AUTH_SIGNATURE = '<%= addons_paypal_embedded_auth_signature %>'
+PAYPAL_EMBEDDED_AUTH_USER = '<%= addons_paypal_embedded_auth_user %>'
+PAYPAL_EMBEDDED_PASSWORD = '<%= addons_paypal_embedded_password %>'
+PAYPAL_EMBEDDED_SIGNATURE = '<%= addons_paypal_embedded_signature %>'
+RESPONSYS_ID = '<%= addons_responsys_id %>'
+SECRET_KEY = '<%= addons_secret_key %>'
+SENTRY_DSN = '<%= addons_sentry_dsn %>'
+WEBAPPS_RECEIPT_KEY = '<%= addons_webapps_receipt_key %>'
+<% if @addons_domain -%>
+DOMAIN = '<%= addons_domain %>'
+<% end -%>
+<% if @addons_static_url -%>
+STATIC_URL = '<%= addons_static_url %>'
+<% end -%>
