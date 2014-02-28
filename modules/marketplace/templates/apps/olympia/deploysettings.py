13bf2075e3320867f591447688a5b0811085a049
Jeremiah Orem <oremj@oremj.com>
2014-02-27 16:40:20 -0800

    fix(olympia): add deploysettings template

---
+++ modules/marketplace/templates/apps/olympia/deploysettings.py
@@ -0,0 +1,14 @@
+CLUSTER = '<%= cluster %>'
+DOMAIN = '<%= domain %>'
+ENV = '<%= env %>'
+SSH_KEY = '<%= ssh_key %>'
+PYREPO = '<%= pyrepo %>'
+CRON_NAME = '<%= cron_name %>'
+GUNICORN = filter(None, '<%= gunicorn %>'.split(';'))
+UWSGI = filter(None, '<%= uwsgi %>'.split(';'))
+MULTI_GUNICORN = filter(None, '<%= multi_gunicorn %>'.split(';'))
+CELERY_SERVICE_PREFIX = '<%= celery_service_prefix %>'
+LOAD_TESTING = <%= load_testing %>
+UPDATE_REF = <% if update_ref %>'<%= update_ref %>'<% else %>None<% end %>
+DEV = <%= dev ? 'True' : 'False' %>
+CRON_USER = '<%= cron_user %>'
