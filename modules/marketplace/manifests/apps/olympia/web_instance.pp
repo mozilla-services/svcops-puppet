44dd6648d81281dc0c86da5e070de774b0897fc3
Jeremiah Orem <oremj@oremj.com>
2014-02-27 17:10:35 -0800

    feat(olympia/web): pass through nginx settings

--- modules/marketplace/manifests/apps/olympia/web_instance.pp
+++ modules/marketplace/manifests/apps/olympia/web_instance.pp
@@ -5,24 +5,26 @@ define marketplace::apps::olympia::web_instance(
   $appmodule = 'wsgi.zamboni:application',
   $workers = 12,
   $worker_class = 'sync',
+  $worker_name = 'addons-olympia-dev',
   $timeout = '90',
   $environ = '',
   $newrelic_license_key = '',
   $newrelic_domain = undef,
-  $user = 'mkt_prod'
+  $nginx_settings = undef,
+  $user = 'mkt_prod',
 ) {
 
   $app_name = $name
 
   if $newrelic_license_key {
     marketplace::newrelic::python { $app_name:
-      before          => Uwsgi::Instance[$app_name],
+      before          => Uwsgi::Instance[$worker_name],
       newrelic_domain => $newrelic_domain,
       license_key     => $newrelic_license_key,
     }
   }
 
-  uwsgi::instance { $app_name:
+  uwsgi::instance { $worker_name:
     app_dir   => "${app_dir}/zamboni",
     appmodule => $appmodule,
     port      => "12${port}",
@@ -30,5 +32,16 @@ define marketplace::apps::olympia::web_instance(
     user      => $user,
     workers   => $workers,
     environ   => $environ,
+  }
+
+  if $nginx_settings {
+    $nginx_resources = {
+      $app_name => $nginx_settings
+    }
+
+    $nginx_defaults = {
+      'webroot' => $app_dir,
+    }
+    create_resources(marketplace::nginx::addons, $nginx_resources, $nginx_defaults)
   }
 }
