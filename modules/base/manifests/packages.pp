4dbbe5f95591f177e7471fea94dd8c163012e0b1
Jeremiah Orem <oremj@oremj.com>
2014-03-03 16:12:03 -0800

    feat(base/packages): add ack globally

--- modules/base/manifests/packages.pp
+++ modules/base/manifests/packages.pp
@@ -1,31 +1,31 @@
 # base packages, we install these everywhere
 class base::packages {
-    package {
-        [
-            'atop',
-            'augeas',
-            'bash-completion',
-            'dstat',
-            'htop',
-            'nano',
-            'nc',
-            'strace',
-            'sysstat',
-            'telnet',
-            'vim-enhanced',
-            'zsh',
-        ]:
-            ensure => latest;
+  package {
+    [
+      'ack',
+      'atop',
+      'augeas',
+      'bash-completion',
+      'dstat',
+      'htop',
+      'nano',
+      'nc',
+      'strace',
+      'sysstat',
+      'telnet',
+      'vim-enhanced',
+      'zsh',
+    ]:
+      ensure => 'latest';
+  }
 
-    }
-
-    ##http://h10025.www1.hp.com/ewfrf/wc/document?cc=us&lc=en&docname=c03465358
-    package {
-        [
-            'glibc.i686',
-            'libstdc++.i686',
-            'libstdc++'
-        ]:
-            ensure => latest;
-    }
+##http://h10025.www1.hp.com/ewfrf/wc/document?cc=us&lc=en&docname=c03465358
+  package {
+    [
+      'glibc.i686',
+      'libstdc++.i686',
+      'libstdc++'
+    ]:
+      ensure => 'latest';
+  }
 }
