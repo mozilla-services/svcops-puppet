a0d8f57a967a750eeb27074ab8af8b34a802b9c5
Jeremiah Orem <oremj@oremj.com>
2014-03-07 15:39:50 -0800

    fix(solitude/deploysettings): proxy should have a different env

--- modules/marketplace/templates/apps/solitude/deploysettings.py
+++ modules/marketplace/templates/apps/solitude/deploysettings.py
@@ -1,13 +1,17 @@
-CLUSTER = '<%= cluster %>'
-DOMAIN = '<%= domain %>'
-ENV = '<%= env %>'
-SSH_KEY = '<%= ssh_key %>'
-CRON_NAME = '<%= cron_name %>'
-CRON_USER = '<%= cron_user %>'
-UWSGI = filter(None, '<%= uwsgi %>'.split(';'))
-PYREPO = '<%= pyrepo %>'
-IS_PROXY = <%= is_proxy ? 'True' : 'False' %>
-WEB_ROLE = '<%= web_role %>'
+CLUSTER = '<%= @cluster %>'
+DOMAIN = '<%= @domain %>'
+<% if @is_proxy -%>
+ENV = 'proxy-<%= @env %>'
+<% else -%>
+ENV = '<%= @env %>'
+<% end -%>
+SSH_KEY = '<%= @ssh_key %>'
+CRON_NAME = '<%= @cron_name %>'
+CRON_USER = '<%= @cron_user %>'
+UWSGI = filter(None, '<%= @uwsgi %>'.split(';'))
+PYREPO = '<%= @pyrepo %>'
+IS_PROXY = <%= @is_proxy ? 'True' : 'False' %>
+WEB_ROLE = '<%= @web_role %>'
 <% if @scl_name -%>
-SCL_NAME = '<%= scl_name %>'
+SCL_NAME = '<%= @scl_name %>'
 <% end -%>
