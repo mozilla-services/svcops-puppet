9e14cdd7a93ddcba9bc42661d2903986d8d87ff7
Jason Thomas <jason@lithiumfox.com>
2014-03-10 14:13:04 -0400

    marketplace/olympia: add symlinks to webroot.

---
+++ modules/marketplace/manifests/apps/olympia/symlinks.pp
@@ -0,0 +1,38 @@
+# manages webroot symlinks
+define marketplace::apps::olympia::symlinks(
+  $netapp
+) {
+  $olympia_webroot = "${name}/default/webroot"
+
+  file {
+    "${olympia_webroot}/media":
+      ensure => 'link',
+      target => '../../media';
+
+    "${olympia_webroot}/favicon.ico":
+      ensure => 'link',
+      target => '../../media/img/favicon.ico';
+
+    "${olympia_webroot}/storage":
+      ensure => 'directory';
+
+    "${olympia_webroot}/storage/shared_storage":
+      ensure => 'directory';
+
+    "${olympia_webroot}/storage/files":
+      ensure => 'link',
+      target => "${netapp}/files";
+
+    "${olympia_webroot}/storage/public-staging":
+      ensure => 'link',
+      target => "${netapp}/public-staging";
+
+    "${olympia_webroot}/storage/shared_storage/ryf":
+      ensure => 'link',
+      target => "${netapp}/shared_storage/ryf";
+
+    "${olympia_webroot}/storage/shared_storage/uploads":
+      ensure => 'link',
+      target => "${netapp}/shared_storage/uploads";
+  }
+}
