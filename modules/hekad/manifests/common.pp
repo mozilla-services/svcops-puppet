27b6daede9d8cfa556b0419adaf6fbe8c6e1d59b
Jason Thomas <jason@lithiumfox.com>
2014-03-10 17:00:52 -0400

    hekad/common: add sandbox_hmac_key

--- modules/hekad/manifests/common.pp
+++ modules/hekad/manifests/common.pp
@@ -1,12 +1,13 @@
 # base heka that some hosts share
 class hekad::common(
-    $udp_listen_address = '127.0.0.1:5565',
-    $statsd_address = '127.0.0.1:8125',
-    $carbon_output_address = '127.0.0.1:2003',
-    $log_output = false,
-  ) {
-    hekad::instance {
-        'hekad':
-          config => template('hekad/hekad.toml.erb');
-    }
+  $carbon_output_address = '127.0.0.1:2003',
+  $log_output = false,
+  $sandbox_hmac_key = '',
+  $statsd_address = '127.0.0.1:8125',
+  $udp_listen_address = '127.0.0.1:5565',
+) {
+  hekad::instance {
+    'hekad':
+      config => template('hekad/hekad.toml.erb');
+  }
 }
