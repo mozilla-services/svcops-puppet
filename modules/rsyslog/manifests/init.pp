8cda40eafcd12cc7a455edf689fd946aa9818868
Jason Thomas <jason@lithiumfox.com>
2014-03-10 13:10:50 -0400

    rsyslog: add support remote_host parameter for PR #17

--- modules/rsyslog/manifests/init.pp
+++ modules/rsyslog/manifests/init.pp
@@ -1,30 +1,32 @@
-class rsyslog {
-    package {
-        'rsyslog':
-            ensure => 'present';
-        'rsyslog-gnutls':
-            ensure => 'present';
-    }
+class rsyslog(
+  $remote_host = undef,
+){
+  package {
+    'rsyslog':
+      ensure => 'installed';
+    'rsyslog-gnutls':
+      ensure => 'installed';
+  }
 
-    file {
-        '/etc/rsyslog.conf':
-            require => Package['rsyslog'],
-            mode => '0644',
-            content => template('rsyslog/rsyslog.conf'),
-            notify => Service['rsyslog'];
+  file {
+    '/etc/rsyslog.conf':
+      require => Package['rsyslog'],
+      mode    => '0644',
+      content => template('rsyslog/rsyslog.conf'),
+      notify  => Service['rsyslog'];
 
-        '/etc/rsyslog.d/':
-            ensure => directory,
-            recurse => true,
-            purge => true;
-    }
+    '/etc/rsyslog.d/':
+      ensure  => 'directory',
+      recurse => true,
+      purge   => true;
+  }
 
-    service {
-        'rsyslog':
-            enable => true,
-            ensure => running,
-            hasrestart => true,
-            hasstatus => true,
-            require => File['/etc/rsyslog.conf'];
-    }
+  service {
+    'rsyslog':
+      ensure     => 'running',
+      enable     => true,
+      hasrestart => true,
+      hasstatus  => true,
+      require    => File['/etc/rsyslog.conf'];
+  }
 }
