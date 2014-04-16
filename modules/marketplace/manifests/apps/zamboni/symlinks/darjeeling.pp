5a1e08c05cd18bde9caae997f75bf16768da5922
Jeremiah Orem <oremj@oremj.com>
2014-04-15 20:56:41 -0700

    marketplace: add darjeeling symlink

---
+++ modules/marketplace/manifests/apps/zamboni/symlinks/darjeeling.pp
@@ -0,0 +1,11 @@
+# manages darjeeling media symlink
+define marketplace::apps::zamboni::symlinks::darjeeling(
+  $darjeeling_dir
+) {
+  $zamboni_media = "${name}/media"
+
+  file {"${zamboni_media}/darjeeling":
+    ensure => 'link',
+    target => "${darjeeling_dir}/darjeeling/src",
+  }
+}
