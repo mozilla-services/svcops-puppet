1444f81f4961289fadf23249160d1090f0d98b37
Jason Thomas <jason@lithiumfox.com>
2014-04-21 09:56:59 -0400

    npmrepo: update to use nginx caching.

--- modules/npmrepo/manifests/init.pp
+++ modules/npmrepo/manifests/init.pp
@@ -1,39 +1,30 @@
 # npmrepo
 class npmrepo(
-    $server_name,
-    $app_dir = '/opt/npm-lazy-mirror',
-    $worker_name = 'npm-lazy-mirror'
+  $cache_dir = '/data/npmrepo',
+  $npm_registry = 'https://registry.npmjs.org/',
+  $ttl = '7d',
 ){
+  $server_name = $name
 
-    $upstream = $worker_name
 
-    include npmrepo::packages
+  file {
+    "${cache_dir}/data":
+      ensure => 'directory',
+      owner  => 'nginx';
 
-    class {
-        'npmrepo::config':
-            server_name => $server_name;
-    }
+    "${cache_dir}/tmp":
+      ensure => 'directory',
+      owner  => 'nginx';
+  }
 
-    nginx::upstream {
-        $upstream:
-            upstream_port => $npmrepo::config::port,
-            require       => Supervisord::Service[$worker_name];
-    }
+  nginx::config{
+    'nginx-cache':
+      content => template('npmrepo/nginx-cache.conf');
+  }
 
-    nginx::serverproxy {
-        $server_name:
-            proxyto => "http://${upstream}";
-    }
-
-    supervisord::service {
-        $worker_name:
-            command => "/usr/bin/node ${app_dir}/server.js -C ${npmrepo::config::config_file} --real_external_port 443 --upstream_use_https",
-            app_dir => $app_dir,
-            user    => 'nobody',
-            require => [
-                Class['npmrepo::config'],
-                Class['npmrepo::packages'],
-            ];
-    }
+  nginx::config{
+    $server_name:
+      content => template('npmrepo/npm-mirror.conf');
+  }
 
 }
