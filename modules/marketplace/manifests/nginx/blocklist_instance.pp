67cdd17451e5f510a955b9eaea89eab372eb8825
Jason Thomas <jason@lithiumfox.com>
2014-04-22 09:43:53 -0400

    marketplace: update blocklist nginx define.

--- modules/marketplace/manifests/nginx/blocklist_instance.pp
+++ modules/marketplace/manifests/nginx/blocklist_instance.pp
@@ -1,5 +1,5 @@
 # blocklist nginx config
-define marketplace::nginx::blocklist(
+define marketplace::nginx::blocklist_instance(
   $server_names # ['blocklist-dev.allizom.org']
 ) {
   $config_name = $name
