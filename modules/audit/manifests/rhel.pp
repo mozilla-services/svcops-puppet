54ad64fa2c5784329de5b63244432c27a550b35a
Jeremiah Orem <oremj@oremj.com>
2014-04-30 11:17:54 -0700

    audit: let yum do its thing

--- modules/audit/manifests/rhel.pp
+++ modules/audit/manifests/rhel.pp
@@ -1,35 +1,20 @@
 class audit::rhel {
   package {
     'audit_package':
-      ensure => 'latest',
+      ensure => 'present',
       name   => 'audit-mozilla',
       notify => Service['auditd'];
     'audispd-mozilla-plugins':
-      ensure => 'latest',
+      ensure => 'present',
       notify => Service['auditd'];
     'audit-mozilla-libs-python':
-      ensure  => 'latest',
+      ensure  => 'present',
       notify  => Service['auditd'],
       require => [ Package['audit_package'], Package['audit-mozilla-libs'] ];
     'audit-mozilla-libs':
-      ensure => 'latest',
+      ensure => 'present',
       notify => Service['auditd'],
       before => Package['audit_package'];
-
-    'audit':
-      ensure => absent,
-      before => Package['audit_package'];
-    'audispd-plugins':
-      ensure => absent,
-      before => Package['audispd-mozilla-plugins'];
-    'audit-libs-python':
-      ensure => absent;
-#        'audit-libs':
-#            ensure => absent,
-#            notify => Service['auditd'],
-#            before => Package['audit-mozilla-libs'];
-    'audispd-plugins-1.8-3.el5.centosmoz5.i386':
-      ensure => absent;
   }
 
 }
