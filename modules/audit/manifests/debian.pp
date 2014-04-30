daae83fc36f2d2fb88a03040f2618b62ba664322
Jason Thomas <jason@lithiumfox.com>
2014-04-30 11:44:21 -0700

    audit: lint

--- modules/audit/manifests/debian.pp
+++ modules/audit/manifests/debian.pp
@@ -1,54 +1,55 @@
+# audit::debian
 class audit::debian {
-    case $::operatingsystemrelease {
-        /^(12\.|3.2.0-36-virtual)/: {
-            $audit_lib = 'libaudit0'
-            $audit_package = '1.7.18-1ubuntu1'
+  case $::operatingsystemrelease {
+    /^(12\.|3.2.0-36-virtual)/: {
+      $audit_lib = 'libaudit0'
+      $audit_package = '1.7.18-1ubuntu1'
 
-            package {
-                'system-config-audit':
-                    ensure => $audit_package;
-            }
-        }
-
-        /^13\./: {
-            $audit_lib = 'libaudit1'
-            $audit_package = '1:2.2.2-1ubuntu4'
-        }
-
-        default: {
-            fail("Audit doesn't support $::operatingsystemrelease")
-        }
+      package {
+        'system-config-audit':
+          ensure => $audit_package;
+      }
     }
 
-    package {
-        # auditd dependencies
-        [
-            'libprelude2',
-            'python-glade2',
-            'menu',
-        ]:
-            ensure => latest;
+    /^13\./: {
+      $audit_lib = 'libaudit1'
+      $audit_package = '1:2.2.2-1ubuntu4'
     }
 
-    package {
-        'audisp-cef':
-            # We would say 1.3 here, but there are complications.
-            # So for now, 'present' is enough. [#997387]
-            ensure => present,
-            require => Package[$audit_lib];
+    default: {
+      fail("Audit doesn't support $::operatingsystemrelease")
     }
+  }
 
-    package {
-        'audit_package':
-            ensure => $audit_package,
-            name   => 'auditd';
+  package {
+    # auditd dependencies
+    [
+      'libprelude2',
+      'menu',
+      'python-glade2',
+    ]:
+      ensure => 'latest'
+  }
 
-        [
-            $audit_lib,
-            'libaudit-dev',
-            'python-audit',
-            'audispd-plugins'
-        ]:
-            ensure => $audit_package;
-    }
+  package {
+    'audisp-cef':
+      # We would say 1.3 here, but there are complications.
+      # So for now, 'present' is enough. [#997387]
+      ensure  => 'present',
+      require => Package[$audit_lib],
+  }
+
+  package {
+    'audit_package':
+      ensure => $audit_package,
+      name   => 'auditd';
+
+    [
+      $audit_lib,
+      'audispd-plugins',
+      'libaudit-dev',
+      'python-audit',
+    ]:
+      ensure => $audit_package;
+  }
 }
--- modules/audit/manifests/rhel5.pp
+++ modules/audit/manifests/rhel5.pp
@@ -1,42 +1,43 @@
+# audit::rhel5
 class audit::rhel5 {
-    package {
-        'audit_package':
-            ensure => 'latest',
-            name   => 'audit-mozilla',
-            notify => Service['auditd'];
+  package {
+    'audit_package':
+      ensure => 'latest',
+      name   => 'audit-mozilla',
+      notify => Service['auditd'];
 
-        'audispd-mozilla-plugins':
-            ensure => 'latest',
-            notify => Service['auditd'];
+    'audispd-mozilla-plugins':
+      ensure => 'latest',
+      notify => Service['auditd'];
 
-        'audit-mozilla-libs-python':
-            ensure => 'latest',
-            notify => Service['auditd'],
-            require => [ Package['audit_package'], Package['audit-mozilla-libs'] ];
+    'audit-mozilla-libs-python':
+      ensure  => 'latest',
+      notify  => Service['auditd'],
+      require => [ Package['audit_package'], Package['audit-mozilla-libs'] ];
 
-        'audit-mozilla-libs':
-            ensure => 'latest',
-            notify => Service['auditd'],
-            before => Package['audit_package'];
+    'audit-mozilla-libs':
+      ensure => 'latest',
+      notify => Service['auditd'],
+      before => Package['audit_package'];
 
-        'audit':
-            ensure => absent,
-            before => Package['audit_package'];
+    'audit':
+      ensure => absent,
+      before => Package['audit_package'];
 
-        'audispd-plugins':
-            ensure => absent,
-            before => Package['audispd-mozilla-plugins'];
+    'audispd-plugins':
+      ensure => absent,
+      before => Package['audispd-mozilla-plugins'];
 
-        'audit-libs-python':
-            ensure => absent;
+    'audit-libs-python':
+      ensure => absent;
 
-#        'audit-libs':
-#            ensure => absent,
-#            notify => Service['auditd'],
-#            before => Package['audit-mozilla-libs'];
+    #        'audit-libs':
+    #            ensure => absent,
+    #            notify => Service['auditd'],
+    #            before => Package['audit-mozilla-libs'];
 
-        'audispd-plugins-1.8-3.el5.centosmoz5.i386':
-            ensure => absent;
-    }
+    'audispd-plugins-1.8-3.el5.centosmoz5.i386':
+      ensure => absent;
+  }
 
 }
