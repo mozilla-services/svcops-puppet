9b9fada9df3bc602ce126bc9eb62a836e2f6c343
Jason Thomas <jason@lithiumfox.com>
2014-05-06 09:50:50 -0400

    marketplace: add yogafire.

---
+++ modules/marketplace/manifests/apps/yogafire/admin_instance.pp
@@ -0,0 +1,30 @@
+# $name is the location of yogafire
+define marketplace::apps::yogafire::admin_instance(
+  $cluster,
+  $domain,
+  $dreadnot_instance,
+  $env,
+  $ssh_key,
+  $project_name = 'yogafire',
+) {
+  $yogafire_dir = $name
+
+  git::clone { $yogafire_dir:
+    repo => 'https://github.com/mozilla/yogafire.git',
+  }
+
+  file {
+    "${yogafire_dir}/deploysettings.py":
+      require => Git::Clone[$yogafire_dir],
+      content => template('marketplace/apps/yogafire/deploysettings.py');
+  }
+
+  dreadnot::stack {
+    $domain:
+      require       => File["${yogafire_dir}/deploysettings.py"],
+      instance_name => $dreadnot_instance,
+      github_url    => 'https://github.com/mozilla/yogafire',
+      git_url       => 'git://github.com/mozilla/yogafire.git',
+      project_dir   => $yogafire_dir;
+  }
+}
