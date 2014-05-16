0ebc6d83d7c1f7f3f053d97575e47ea234a5dbf5
Jason Thomas <jason@lithiumfox.com>
2014-05-16 10:07:34 -0400

    marketplace/nginx: add addons_hostname to blocklist nginx config.

--- modules/marketplace/manifests/nginx/blocklist_instance.pp
+++ modules/marketplace/manifests/nginx/blocklist_instance.pp
@@ -1,6 +1,7 @@
 # blocklist nginx config
 define marketplace::nginx::blocklist_instance(
-  $server_names # ['blocklist-dev.allizom.org']
+  $server_names, # ['blocklist-dev.allizom.org']
+  $addons_hostname = 'addons.mozilla.org',
 ) {
   $config_name = $name
 
