0e68c2173201db69a9fc4fe10cee4d1989c6ae91
Jeremiah Orem <oremj@oremj.com>
2014-05-19 14:39:07 -0700

    monolith/firewall: allow multicast

--- modules/marketplace/manifests/apps/monolith/web/firewall.pp
+++ modules/marketplace/manifests/apps/monolith/web/firewall.pp
@@ -6,32 +6,36 @@ class marketplace::apps::monolith::web::firewall {
   }
 
   include firewall
-  firewall {
-    '100 allow established/related':
-      action  => 'accept',
-      ctstate => ['ESTABLISHED', 'RELATED'],
-      proto   => 'all';
-
-    '110 allow ICMP':
-      action => 'accept',
-      proto  => 'icmp';
-
-    '120 allow lo':
-      action  => 'accept',
-      iniface => 'lo',
-      proto   => 'all';
-
-    '200 allow ssh':
-      action => 'accept',
-      dport  => '22';
-
-    '201 allow http':
-      action => 'accept',
-      dport  => ['80-81'];
-
-    '300 log input':
-      jump       => 'LOG',
-      log_prefix => 'WILLDROP: ',
-      proto      => 'all';
+  firewall { '100 allow established/related':
+    action  => 'accept',
+    ctstate => ['ESTABLISHED', 'RELATED'],
+    proto   => 'all',
+  }->
+  firewall { '110 allow ICMP':
+    action => 'accept',
+    proto  => 'icmp',
+  }->
+  firewall { '120 allow lo':
+    action  => 'accept',
+    iniface => 'lo',
+    proto   => 'all',
+  }->
+  firewall { '130 allow mcast':
+    action  => 'accept',
+    pkttype => 'multicast',
+    proto   => 'all',
+  }->
+  firewall { '200 allow ssh':
+    action => 'accept',
+    dport  => '22',
+  }->
+  firewall { '201 allow http':
+    action => 'accept',
+    dport  => ['80-81'],
+  }->
+  firewall { '300 log input':
+    jump       => 'LOG',
+    log_prefix => 'WILLDROP: ',
+    proto      => 'all',
   }
 }
