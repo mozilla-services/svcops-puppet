3eb89b95f7fddb0e273da4a43c9822064a86e663
Jeremiah Orem <oremj@oremj.com>
2014-05-19 15:37:41 -0700

    monolith: allow ganglia and http on subnet

--- modules/marketplace/manifests/apps/monolith/web/firewall.pp
+++ modules/marketplace/manifests/apps/monolith/web/firewall.pp
@@ -2,9 +2,25 @@
 class marketplace::apps::monolith::web::firewall {
   require base::firewall::pre
 
-  firewall { '201 allow http':
+  firewall { '400 allow subnet http':
     action => 'accept',
-    dport  => ['80-81'],
+    chain  => 'SUBNET',
+    dport  => ['80-82'],
+  }->
+  firewall { '401 allow subnet ganglia':
+    action => 'accept',
+    chain  => 'SUBNET',
+    dport  => '8649',
+  }
+
+  firewall { '300 allow http':
+    action => 'accept',
+    dport  => ['80-82'],
+  }->
+  firewall { '301 allow subnet ganglia':
+    action => 'accept',
+    chain  => 'SUBNET',
+    dport  => '8649',
   }->
   firewall { '990 log input':
     jump       => 'LOG',
