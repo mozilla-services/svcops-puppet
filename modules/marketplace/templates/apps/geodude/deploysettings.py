2f2923501d6c7926dcc62d8b9e4a3f83f437695d
Jason Thomas <jason@lithiumfox.com>
2014-05-23 18:00:49 -0400

    marketplace/geodude: add UWSGI.

--- modules/marketplace/templates/apps/geodude/deploysettings.py
+++ modules/marketplace/templates/apps/geodude/deploysettings.py
@@ -4,4 +4,4 @@ ENV = '<%= @env %>'
 SSH_KEY = '<%= @ssh_key %>'
 PYREPO = '<%= @pyrepo %>'
 UPDATE_REF = <% if update_ref %>'<%= @update_ref %>'<% else %>None<% end %>
-UWSGI = '<%= @geodude %>'
+UWSGI = '<%= @uwsgi %>'
