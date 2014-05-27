5ff187ff4f8aefc406a0e239c59b70df534fe6cf
Jason Thomas <jason@lithiumfox.com>
2014-05-27 11:40:29 -0400

    timezone: exec tzdata-update when clock file is updated.

--- modules/timezone/manifests/init.pp
+++ modules/timezone/manifests/init.pp
@@ -7,14 +7,27 @@ class timezone(
     'tzdata':
       ensure => 'installed';
   }
+
   file {
     '/etc/localtime':
       require => Package['tzdata'],
-      target  => "/usr/share/zoneinfo/${timezone}";
+      target  => "/usr/share/zoneinfo/${timezone}",
+  }
 
+  file {
     '/etc/sysconfig/clock':
       ensure  => 'file',
       content => "ZONE=\"${timezone}\"\n",
+      require => Package['tzdata'],
+      notify  => Exec['tzdata-update'],
+  }
+
+  exec {
+    'tzdata-update':
+      command     => '/usr/sbin/tzdata-update',
+      refreshonly => true,
+      logoutput   => 'on_failure',
+      require     => Package['tzdata'],
 
   }
 }
