4167782df301f76657f275aa11b6bba75d777e67
Jason Thomas <jason@lithiumfox.com>
2014-06-12 10:02:18 -0400

    modprobe: fix namespace conflict.

--- modules/modprobe/manifests/persistent.pp
+++ modules/modprobe/manifests/persistent.pp
@@ -3,20 +3,20 @@ define modprobe::persistent(
   $ensure = 'file',
 ){
 
-  $module_dir = '/etc/sysconfig/modules'
-  $module_name = $name
+  $mod_dir = '/etc/sysconfig/modules'
+  $mod_name = $name
 
   file {
-    "${module_dir}/${module_name}.modules":
+    "${mod_dir}/${mod_name}.modules":
       ensure  => $ensure,
       content => template('modprobe/template.modules'),
       mode    => '0755',
   }
 
   exec {
-    "modprobe-${module_name}":
-      command => "/sbin/modprobe ${module_name}",
-      unless  => "/sbin/lsmod | /bin/grep ${module_name} 2>/dev/null",
+    "modprobe-${mod_name}":
+      command => "/sbin/modprobe ${mod_name}",
+      unless  => "/sbin/lsmod | /bin/grep ${mod_name} 2>/dev/null",
   }
 
 }
