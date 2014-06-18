2ecbce1b1010ec66d8fc5577ca5aaf26fcddf25a
Jason Thomas <jason@lithiumfox.com>
2014-06-18 17:06:34 -0400

    memcached: add multiple memcached support.

---
+++ modules/memcached/manifests/multi_instance.pp
@@ -0,0 +1,33 @@
+# memcached multi_instance
+define memcached::multi_instance (
+  $cachesize,
+  $port,
+  $max_connections = '20048',
+  $threads = '12',
+) {
+
+  include memcached::package
+
+  file {
+    "/etc/sysconfig/memcached_${port}":
+      ensure  => 'file',
+      notify  => Service["memcached_${port}"],
+      content => template('memcached/memcached');
+
+    "/etc/init.d/memcached_${port}":
+      ensure  => 'file',
+      content => template('memcached/init.d/memcached'),
+      mode    => '0755';
+  }
+
+  service {
+    "memcached_${port}":
+      ensure  => 'running',
+      enable  => true,
+      require => [
+        Class['memcached::package'],
+        File["/etc/init.d/memcached_${port}"],
+        File["/etc/sysconfig/memcached_${port}"],
+      ],
+  }
+}
