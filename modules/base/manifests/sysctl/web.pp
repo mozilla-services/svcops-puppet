fe3c62ea3f13abf42ce822a183fb51ed960f25e9
Jason Thomas <jason@lithiumfox.com>
2014-06-18 10:51:41 -0400

    base:sysctl: update web sysctl settings.

--- modules/base/manifests/sysctl/web.pp
+++ modules/base/manifests/sysctl/web.pp
@@ -9,6 +9,9 @@ class base::sysctl::web {
     'net.ipv4.tcp_max_syn_backlog':
       value => '100000';
 
+    'net.core.netdev_max_backlog':
+      value => '60000';
+
     'net.ipv4.tcp_congestion_control':
       value   => 'htcp',
       require => Modprobe::Persistent['tcp_htcp'];
@@ -17,19 +20,31 @@ class base::sysctl::web {
       value => '40960';
 
     'net.ipv4.tcp_wmem':
-      value => '4096 87380 16777216';
+      value => '8192 873800 16777216';
 
     'net.ipv4.tcp_rmem':
-      value => '4096 87380 16777216';
+      value => '8192 873800 16777216';
 
     'net.core.rmem_max':
       value => '16777216';
 
     'net.core.wmem_max':
-      value =>'16777216';
+      value => '16777216';
 
-    'net.ipv4.ip_local_port_range':
-      value => '12000 64000';
+    'net.core.wmem_default':
+      value => '65536';
+
+    'net.core.rmem_default':
+      value => '65536';
+
+    'net.ipv4.tcp_tw_reuse':
+      value => '1';
+
+    'net.ipv4.tcp_fin_timeout':
+      value => '15';
+
+    'net.ipv4.tcp_syncookies':
+      value => '0';
 
     'net.ipv4.tcp_keepalive_time':
       value => '120';
@@ -37,10 +52,10 @@ class base::sysctl::web {
     'net.ipv4.tcp_keepalive_intvl':
       value => '40';
 
-    'net.ipv4.tcp_tw_reuse':
-      value => '1';
+    'net.ipv4.tcp_keepalive_probes':
+      value => '20';
 
-    'net.ipv4.tcp_fin_timeout':
-      value => '15';
+    'net.ipv4.ip_local_port_range':
+      value => '12000 64000';
   }
 }
