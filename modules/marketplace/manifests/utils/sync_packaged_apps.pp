99792c5a27db38e2f9976199c3c751cb21b420f8
Jeremiah Orem <oremj@oremj.com>
2014-07-07 09:43:08 -0700

    marketplace/sync_packaged_apps: use DJANGO_SETTINGS_MODULE

--- modules/marketplace/manifests/utils/sync_packaged_apps.pp
+++ modules/marketplace/manifests/utils/sync_packaged_apps.pp
@@ -7,25 +7,27 @@ class marketplace::utils::sync_packaged_apps(
   $zamboni = '/data/mkt.prod/www/marketplace.firefox.com/current/zamboni',
 ) {
 
+  $cron_prefix = "cd ${zamboni}; DJANGO_SETTINGS_MODULE=settings_local_mkt ../venv/bin/python manage.py list_packaged_apps"
+
   file { '/root/bin/sync_packaged_apps':
     content => template('marketplace/utils/sync_packaged_apps.py'),
     mode    => '0700',
   }->
   cron {
     'sync_packaged_apps-public':
-      command => "cd ${zamboni}; ../venv/bin/python manage.py --settings=settings_local_mkt list_packaged_apps --status=public | /root/bin/sync_packaged_apps public > /dev/null",
+      command => "${cron_prefix} --status=public | /root/bin/sync_packaged_apps public > /dev/null",
       minute  => '7';
 
     'sync_packaged_apps-pending':
-      command => "cd ${zamboni}; ../venv/bin/python manage.py --settings=settings_local_mkt list_packaged_apps --status=pending | /root/bin/sync_packaged_apps pending > /dev/null",
+      command => "${cron_prefix} --status=pending | /root/bin/sync_packaged_apps pending > /dev/null",
       minute  => '14';
 
     'sync_packaged_apps-approved':
-      command => "cd ${zamboni}; ../venv/bin/python manage.py --settings=settings_local_mkt list_packaged_apps --status=approved | /root/bin/sync_packaged_apps approved > /dev/null",
+      command => "${cron_prefix} --status=approved | /root/bin/sync_packaged_apps approved > /dev/null",
       minute  => '21';
 
     'sync_packaged_apps-rejected':
-      command => "cd ${zamboni}; ../venv/bin/python manage.py --settings=settings_local_mkt list_packaged_apps --status=rejected | /root/bin/sync_packaged_apps rejected > /dev/null",
-      minute  => '7';
+      command => "${cron_prefix} --status=rejected | /root/bin/sync_packaged_apps rejected > /dev/null",
+      minute  => '28';
   }
 }
