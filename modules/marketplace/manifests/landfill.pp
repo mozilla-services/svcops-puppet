27ecd13f70a7eb1abd31fb4619acb56de0cd7101
Jeremiah Orem <oremj@oremj.com>
2014-08-04 13:15:56 -0700

    Bug 1048484 - Disable landfill web frontends

--- modules/marketplace/manifests/landfill.pp
+++ modules/marketplace/manifests/landfill.pp
@@ -1,5 +1,11 @@
 # landfill requirements
 class marketplace::landfill {
+  file { '/data/landfilldisabled':
+    ensure => 'directory',
+  }
+  file { '/data/landfilldisabled/disabled.txt':
+    content => 'landfill is currently offline. Join us in #amo and #marketplace for AMO and Marketplace development chat.',
+  }
   file { '/usr/bin/landfill_dump':
     content => template('marketplace/landfill/landfill_dump'),
     mode    => '0755',
