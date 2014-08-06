c5ff6dce6a01a31819356cf356622521163073ac
Jeremiah Orem <oremj@oremj.com>
2014-08-06 12:04:56 -0700

    go_freddo: initial

---
+++ modules/go_freddo/manifests/branch.pp
@@ -0,0 +1,11 @@
+# go_freddo branch
+define go_freddo::branch(
+  $app,
+  $branch_ref,
+  $script,
+) {
+  $branch_name = $name
+  concat::fragment { "go_freddo_branch_${branch_name}":
+    target  => '/etc/go-freddo.toml',
+    content => "[[apps.${app}.branch]]\nref = \"${branch_ref}\"\nscript = \"${script}\"\n",
+}
