c641b5fd9a9e1be577eba954b2169d51487d0608
Jeremiah Orem <oremj@oremj.com>
2014-08-06 14:14:20 -0700

    go_freddo/branch: missing }

--- modules/go_freddo/manifests/branch.pp
+++ modules/go_freddo/manifests/branch.pp
@@ -9,4 +9,5 @@ define go_freddo::branch(
   concat::fragment { "go_freddo_branch_${branch_name}":
     target  => '/etc/go-freddo.toml',
     content => "[[apps.${app}.branch]]\nref = \"${branch_ref}\"\nscript = \"${script}\"\n\n",
+  }
 }
