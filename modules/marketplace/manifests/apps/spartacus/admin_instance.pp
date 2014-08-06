59872ff742c2b9a40f815095cab43c309c90d083
Jeremiah Orem <oremj@oremj.com>
2014-08-06 15:42:18 -0700

    update_on_commit: fix name conflicts

--- modules/marketplace/manifests/apps/spartacus/admin_instance.pp
+++ modules/marketplace/manifests/apps/spartacus/admin_instance.pp
@@ -34,7 +34,7 @@ define marketplace::apps::spartacus::admin_instance(
   }
 
   if $update_on_commit {
-    go_freddo::branch { "${codename}_${env}":
+    go_freddo::branch { "${codename}_${domain}_${env}":
       app    => $codename,
       script => "/usr/local/bin/dreadnot.deploy -e dev ${domain}",
     }
