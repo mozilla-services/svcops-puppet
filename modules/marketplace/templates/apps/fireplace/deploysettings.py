13661df541739b9042aca945dcd0bf4c9cb9e117
Jason Thomas <jason@lithiumfox.com>
2014-08-07 16:38:26 -0400

    marketplace:apps:fireplace: remove new line.

--- modules/marketplace/templates/apps/fireplace/deploysettings.py
+++ modules/marketplace/templates/apps/fireplace/deploysettings.py
@@ -4,6 +4,6 @@ ENV = '<%= @env %>'
 SSH_KEY = '<%= @ssh_key %>'
 PROJECT_NAME = '<%= @project_name %>'
 ZAMBONI_DIR = '<%= @zamboni_dir %>'
-<% if @package_name %>
-PACKAGE_NAME = <%= @package_name %>
+<% if @package_name -%>
+PACKAGE_NAME = '<%= @package_name %>'
 <% end -%>
