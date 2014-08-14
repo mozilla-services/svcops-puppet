be56786d8e212b631d8bdcec4020c32b3d960d50
Jeremiah Orem <oremj@oremj.com>
2014-08-14 10:21:26 -0700

    testmanifest: add wildcard proxy

--- modules/marketplace/manifests/apps/testmanifest/web_instance.pp
+++ modules/marketplace/manifests/apps/testmanifest/web_instance.pp
@@ -11,11 +11,15 @@ define marketplace::apps::testmanifest::web_instance(
     ensure => 'installed',
   }->
   supervisord::service { "testmanifest-${domain}":
-    app_dir => "${project_dir}",
+    app_dir => "${project_dir}/testmanifest",
     command => "/usr/bin/node index.js -p ${port}",
     user    => $user,
   }->
   nginx::serverproxy { $domain:
+    listen  => '81',
+    proxyto => "http://localhost:${port}",
+  }
+  nginx::serverproxy { "*.${domain}":
     listen  => '81',
     proxyto => "http://localhost:${port}",
   }
