cdec315c562f178936de9c430a497a4e93ed287f
Jeremiah Orem <oremj@oremj.com>
2014-08-26 09:26:48 -0700

    comment out rhn cert

--- modules/base/manifests/rhn.pp
+++ modules/base/manifests/rhn.pp
@@ -1,8 +1,10 @@
 # Manage rhn related files
 class base::rhn {
+  /*
   file {
     '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT':
       ensure  => present,
       content => template('base/RHN-ORG-TRUSTED-SSL-CERT')
   }
+  */
 }
