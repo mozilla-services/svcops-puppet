39fd39ace09f9809502b8e6756019bc44d3e5990
Jason Thomas <jason@lithiumfox.com>
2014-09-18 16:09:48 -0400

    frappe: manage local settings.

--- modules/marketplace/manifests/apps/frappe/admin_instance.pp
+++ modules/marketplace/manifests/apps/frappe/admin_instance.pp
@@ -1,19 +1,25 @@
 # $name is the location of frappe
 define marketplace::apps::frappe::admin_instance(
+  $caches_default_location,
   $cluster,
+  $databases_default_url,
   $domain,
   $dreadnot_instance,
   $env,
+  $secret_key,
   $ssh_key,
+  $max_threads = '2',
   $data_path = undef,
   $project_name = 'frappe',
   $pyrepo = 'https://pyrepo.addons.mozilla.org/',
   $scl_name = undef,
   $update_on_commit = false,
   $user = 'nobody',
+
 ) {
   $frappe_dir = $name
   $codename = 'frappe'
+  $cache_prefix = md5($domain)
 
   git::clone { $frappe_dir:
     repo => 'https://github.com/grafos-ml/frappe',
@@ -24,6 +30,8 @@ define marketplace::apps::frappe::admin_instance(
       content => template('marketplace/apps/frappe/admin/deploysettings.py');
     "${frappe_dir}/fabfile.py":
       content => template('marketplace/apps/frappe/admin/fabfile.py');
+    "${frappe_dir}/src/recommendation/local.py":
+      content => template('marketplace/apps/frappe/admin/local.py');
   }->
   dreadnot::stack {
     $domain:
---
+++ modules/marketplace/templates/apps/frappe/admin/local.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import dj_database_url
+
+from default_settings import *
+
+TEMPLATE_DEBUG = False
+
+TESTING_MODE = False
+
+TIME_ZONE = 'UTC'
+
+SECRET_KEY = '<%= @secret_key %>'
+
+MAX_THREADS = <%= @max_threads %>
+
+ALLOWED_HOSTS = '<%= @domain %>'
+
+DATABASES = {}
+DATABASES_DEFAULT_URL = '<%= @databases_default_url %>'
+DATABASES['default'] = dj_database_url.parse(DATABASES_DEFAULT_URL)
+DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+DATABASES['default']['OPTIONS'] = {'init_command': 'SET storage_engine=InnoDB'}
+
+
+CACHE_PREFIX = '<%= cache_prefix %>'
+CACHES_DEFAULT_LOCATION = '<%= @caches_default_location %>'
+
+CACHES = {
+    'default': {
+        'BACKEND': "django.core.cache.backends.memcached.MemcachedCache",
+        'LOCATION': splitstrip(CACHES_DEFAULT_LOCATION),
+        'TIMEOUT': 500,
+        'KEY_PREFIX': CACHE_PREFIX,
+        }
+    }
+
+CACHES['distributed'] = CACHES['default']
+
+
+def splitstrip(s):
+    return [x.strip() for x in s.split(';')]
