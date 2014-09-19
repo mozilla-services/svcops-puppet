98899a3f3776bf3509ccdfcd3bc0d2502f2d0a24
Jason Thomas <jason@lithiumfox.com>
2014-09-19 16:17:25 -0400

    marketplace:frappe: restart uwsgi service on deploy

--- modules/marketplace/templates/apps/frappe/admin/deploysettings.py
+++ modules/marketplace/templates/apps/frappe/admin/deploysettings.py
@@ -5,6 +5,7 @@ SSH_KEY = '<%= @ssh_key %>'
 PROJECT_NAME = '<%= @project_name %>'
 PYREPO = '<%= @pyrepo %>'
 DATA_PATH = '<%= @data_path %>'
+UWSGI = filter(None, '<%= @uwsgi %>'.split(';'))
 <% if @scl_name -%>
 SCL_NAME = '<%= scl_name %>'
 <% end -%>
