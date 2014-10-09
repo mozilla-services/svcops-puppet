2946df467d8c1282f34312b3842f6d62e3c3cec8
Jason Thomas <jason@lithiumfox.com>
2014-10-09 11:19:35 -0400

    base:rhn: only on rhel6.

--- modules/base/manifests/rhn.pp
+++ modules/base/manifests/rhn.pp
@@ -1,9 +1,11 @@
 # Manage rhn related files
 class base::rhn {
-  $rhn_cert = hiera('rhn_ssl_cert', '')
-  if $rhn_cert == '' {
-    file { '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT':
-      content => template('base/RHN-ORG-TRUSTED-SSL-CERT'),
+  if $::operatingsystem == 'RedHat' {
+    $rhn_cert = hiera('rhn_ssl_cert', '')
+    if $rhn_cert == '' {
+      file { '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT':
+        content => template('base/RHN-ORG-TRUSTED-SSL-CERT'),
+      }
     }
   }
 }
