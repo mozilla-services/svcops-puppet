6706bf546dbc795f769b6172bb2b5c8746bb4e83
Jeremiah Orem <oremj@oremj.com>
2015-01-14 10:07:06 -0800

    zamboni/aeskeys: add overlay

--- modules/marketplace/manifests/apps/zamboni/aeskeys.pp
+++ modules/marketplace/manifests/apps/zamboni/aeskeys.pp
@@ -1,6 +1,9 @@
 # name is the root of the zamboni checkout.
 define marketplace::apps::zamboni::aeskeys(
   $preverified_account_key,
+
+  $cluster = undef,
+  $env = undef,
 ) {
   $project_root = $name
   file { "${project_root}/aeskeys":
@@ -9,5 +12,19 @@ define marketplace::apps::zamboni::aeskeys(
   file {
     "${project_root}/aeskeys/preverified_account.key":
       content => $preverified_account_key;
+  }
+
+  if $cluster and $env {
+    Marketplace::Overlay {
+      app     => 'zamboni-aeskeys',
+      cluster => $cluster,
+      env     => $env,
+    }
+
+    marketplace::overlay {
+      "zamboni-aeskeys::keys::${name}::preverified_account.key":
+        content  => $preverified_account_key,
+        filename => 'preverified_account.key';
+    }
   }
 }
