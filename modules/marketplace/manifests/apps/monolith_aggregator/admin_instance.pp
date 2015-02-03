8c2c66d825e77a9f17db5df33ef9d32f25f56bd8
Jason Thomas <jason@lithiumfox.com>
2015-02-03 12:01:57 -0500

    marketplace:monolith_aggregator: add overlay support.

--- modules/marketplace/manifests/apps/monolith_aggregator/admin_instance.pp
+++ modules/marketplace/manifests/apps/monolith_aggregator/admin_instance.pp
@@ -1,86 +1,114 @@
 # define admin instance for monolith aggregator
 define marketplace::apps::monolith_aggregator::admin_instance(
-    $mkt_endpoint,
-    $mkt_transaction_endpoint,
-    $db_uri,
-    $es_url,
-    $es_index_prefix,
-    $ga_auth,
-    $mkt_user,
-    $mkt_pass,
-    $solitude_access_key,
-    $solitude_secret_key,
-    $solitude_bucket,
-    $cluster,
-    $domain,
-    $env,
-    $ssh_key,
-    $dreadnot_name,
-    $dreadnot_instance,
-    $log_level = 'debug',
-    $es_index_prefix = undef,
-    $cron_user = 'mkt_prod_monolith',
-    $pyrepo = 'https://pyrepo.addons.mozilla.org/',
-    $update_on_commit = false,
+  $mkt_endpoint,
+  $mkt_transaction_endpoint,
+  $db_uri,
+  $es_url,
+  $es_index_prefix,
+  $ga_auth,
+  $mkt_user,
+  $mkt_pass,
+  $solitude_access_key,
+  $solitude_secret_key,
+  $solitude_bucket,
+  $cluster,
+  $domain,
+  $env,
+  $ssh_key,
+  $dreadnot_name,
+  $dreadnot_instance,
+  $dreadnot = false,
+  $log_level = 'debug',
+  $es_index_prefix = undef,
+  $cron_user = 'mkt_prod_monolith',
+  $pyrepo = 'https://pyrepo.addons.mozilla.org/',
+  $update_on_commit = false,
 ) {
   $codename = 'monolith-aggregator'
-    $project_dir = $name
-    $installed_dir = regsubst($project_dir, 'src', 'www')
-    git::clone {
-        "${project_dir}/monolith-aggregator":
-            repo => 'https://github.com/mozilla/monolith-aggregator.git';
+  $project_dir = $name
+  $installed_dir = regsubst($project_dir, 'src', 'www')
+  git::clone {
+    "${project_dir}/monolith-aggregator":
+      repo => 'https://github.com/mozilla/monolith-aggregator.git';
 
-    }
-    
+  }
 
-    $log_file = "/var/log/${domain}.log"
 
-    # make sure log file is owned by the cron user
-    file {
-        $log_file:
-            ensure => present,
-            owner  => $cron_user
-    }
+  $log_file = "/var/log/${domain}.log"
 
-    cron {
-        "aggr-${project_dir}":
-            environment => 'MAILTO=amo-developers@mozilla.org',
-            command     => "cd ${installed_dir}/current/monolith-aggregator; ../venv/bin/monolith-extract aggregator.ini --log-level ${log_level} --start-date `/bin/date --date='yesterday' +'\\%Y-\\%m-\\%d'` --end-date `/bin/date --date='yesterday' +'\\%Y-\\%m-\\%d'` > ${log_file} 2>&1",
-            user        => $cron_user,
-            hour        => 9,
-            minute      => 15;
-    }
+  # make sure log file is owned by the cron user
+  file {
+    $log_file:
+      ensure => present,
+      owner  => $cron_user
+  }
 
-    file {
-        "${project_dir}/monolith-aggregator/aggregator.ini":
-            require => Git::Clone["${project_dir}/monolith-aggregator"],
-            content => template('marketplace/apps/monolith_aggregator/admin/aggregator.ini');
+  cron {
+    "aggr-${project_dir}":
+      environment => 'MAILTO=amo-developers@mozilla.org',
+      command     => "cd ${installed_dir}/current/monolith-aggregator; ../venv/bin/monolith-extract aggregator.ini --log-level ${log_level} --start-date `/bin/date --date='yesterday' +'\\%Y-\\%m-\\%d'` --end-date `/bin/date --date='yesterday' +'\\%Y-\\%m-\\%d'` > ${log_file} 2>&1",
+      user        => $cron_user,
+      hour        => 9,
+      minute      => 15;
+  }
 
-        "${project_dir}/monolith-aggregator/auth.json":
-            require => Git::Clone["${project_dir}/monolith-aggregator"],
-            content => $ga_auth;
+  Marketplace::Overlay {
+    app     => 'monolith-aggregator',
+    cluster => $cluster,
+    env     => $env,
+  }
 
-        "${project_dir}/monolith-aggregator/monolith.password.ini":
-            require => Git::Clone["${project_dir}/monolith-aggregator"],
-            content => template('marketplace/apps/monolith_aggregator/admin/monolith.password.ini');
+  marketplace::overlay {
+    "monolith_aggregator::deploysettings::${name}":
+      content  => template('marketplace/apps/monolith/deploysettings.py'),
+      filename => 'deploysettings.py';
 
-        "${project_dir}/monolith-aggregator/solitude_aws_keys.ini":
-            require => Git::Clone["${project_dir}/monolith-aggregator"],
-            content => template('marketplace/apps/monolith_aggregator/admin/solitude_aws_keys.ini');
-    }
+    "monolith_aggregator::setttings::aggregator::${name}":
+      content  => template('marketplace/apps/monolith_aggregator/admin/aggregator.ini'),
+      filename => 'aggregator.ini';
 
-    file {
-        "${project_dir}/monolith-aggregator/deploysettings.py":
-            require => Git::Clone["${project_dir}/monolith-aggregator"],
-            content => template('marketplace/apps/monolith/deploysettings.py');
-    }
+    "monolith_aggregator::setttings::auth::${name}":
+      content  => $ga_auth,
+      filename => 'auth.json';
 
+    "monolith_aggregator::setttings::password::${name}":
+      content  => template('marketplace/apps/monolith_aggregator/admin/monolith.password.ini'),
+      filename => 'monolith.password.ini';
+
+    "monolith_aggregator::setttings::solitude_aws::${name}":
+      content  => template('marketplace/apps/monolith_aggregator/admin/solitude_aws_keys.ini'),
+      filename => 'solitude_aws_keys.ini';
+  }
+
+  file {
+    "${project_dir}/monolith-aggregator/aggregator.ini":
+      require => Git::Clone["${project_dir}/monolith-aggregator"],
+      content => template('marketplace/apps/monolith_aggregator/admin/aggregator.ini');
+
+    "${project_dir}/monolith-aggregator/auth.json":
+      require => Git::Clone["${project_dir}/monolith-aggregator"],
+      content => $ga_auth;
+
+    "${project_dir}/monolith-aggregator/monolith.password.ini":
+      require => Git::Clone["${project_dir}/monolith-aggregator"],
+      content => template('marketplace/apps/monolith_aggregator/admin/monolith.password.ini');
+
+    "${project_dir}/monolith-aggregator/solitude_aws_keys.ini":
+      require => Git::Clone["${project_dir}/monolith-aggregator"],
+      content => template('marketplace/apps/monolith_aggregator/admin/solitude_aws_keys.ini');
+
+    "${project_dir}/monolith-aggregator/deploysettings.py":
+      require => Git::Clone["${project_dir}/monolith-aggregator"],
+      content => template('marketplace/apps/monolith/deploysettings.py');
+  }
+
+  if $dreadnot {
     dreadnot::stack {
-        $dreadnot_name:
-            instance_name => $dreadnot_instance,
-            project_dir   => "${project_dir}/monolith-aggregator",
-            github_url    => 'https://github.com/mozilla/monolith-aggregator',
-            git_url       => 'git://github.com/mozilla/monolith-aggregator.git';
+      $dreadnot_name:
+        instance_name => $dreadnot_instance,
+        project_dir   => "${project_dir}/monolith-aggregator",
+        github_url    => 'https://github.com/mozilla/monolith-aggregator',
+        git_url       => 'git://github.com/mozilla/monolith-aggregator.git';
     }
     if $update_on_commit {
       go_freddo::branch { "${codename}_${domain}_${env}":
@@ -88,4 +116,5 @@ define marketplace::apps::monolith_aggregator::admin_instance(
         script => "/usr/local/bin/dreadnot.deploy -e ${dreadnot_instance} ${domain}",
       }
     }
+  }
 }
