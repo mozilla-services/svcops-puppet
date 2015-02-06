5bf7dde72f91c62d4184cff3e75937547febe5e4
Jason Thomas <jason@lithiumfox.com>
2015-02-06 13:50:26 -0500

    marketplace:solitude: add overlays.

--- modules/marketplace/manifests/apps/solitude/settings.pp
+++ modules/marketplace/manifests/apps/solitude/settings.pp
@@ -21,9 +21,49 @@ define marketplace::apps::solitude::settings(
   $bango_basic_auth,
   $s3_auth_key='',
   $s3_auth_secret='',
-  $s3_bucket=''
+  $s3_bucket='',
+  $cluster = undef,
+  $env = undef,
 ) {
   $settings_dir = "${project_dir}/solitude/solitude/settings"
+
+  if $cluster and $env {
+    Marketplace::Overlay {
+      app     => 'solitude',
+      cluster => $cluster,
+      env     => $env,
+    }
+
+    marketplace::overlay {
+      "solitude::settings::${name}/solitude":
+        ensure   => 'directory',
+        filename => 'solitude';
+      "solitude::settings::${name}/solitude/solitude":
+        ensure   => 'directory',
+        filename => 'solitude/solitude';
+
+      "solitude::settings::${name}/solitude/settings":
+        ensure   => 'directory',
+        filename => 'solitude/solitude/settings';
+
+      "solitude::settings::${name}/solitude/settings/sites":
+        ensure   => 'directory',
+        filename => 'solitude/solitude/settings/sites';
+
+      "solitude::settings::${name}/solitude/settings/sites/${site}":
+        ensure   => 'directory',
+        filename => "solitude/solitude/settings/sites/${site}";
+
+      "solitude::settings::${name}/solitude/settings/local.py":
+        content  => "from .sites.${site}.db import *\n",
+        filename => 'solitude/solitude/settings/local.py';
+
+      "solitude::settings::${name}/solitude/settings/sites/${site}/private_base.py":
+        content  => template('marketplace/apps/solitude/settings.py'),
+        filename => "solitude/solitude/settings/sites/${site}/private_base.py";
+    }
+  }
+
   file {
     "${settings_dir}/local.py":
       content => "from .sites.${site}.db import *\n";
