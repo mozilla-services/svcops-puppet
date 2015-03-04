92ea8fc69913306dd35bc5f27fc605c77abd2fb9
Jason Thomas <jason@lithiumfox.com>
2015-03-04 12:39:49 -0500

    marketplace:marketplace_style_guide: $domain = $name.

--- modules/marketplace/manifests/apps/marketplace_style_guide/admin_instance.pp
+++ modules/marketplace/manifests/apps/marketplace_style_guide/admin_instance.pp
@@ -1,12 +1,12 @@
 # $name is the location of style_guide
 define marketplace::apps::marketplace_style_guide::admin_instance(
   $cluster,
-  $domain,
   $env,
   $ssh_key,
   $project_name = 'marketplace-style-guide',
 ) {
-  $marketplace_style_guide_dir = $name
+
+  $domain = $name
 
   marketplace::overlay { "marketplace_style_guide::deploysettings::${name}":
     app      => $project_name,
